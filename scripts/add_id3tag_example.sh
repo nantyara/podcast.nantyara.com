337d90b977b818ab99988040d1d3ac0588a055da
Shinta Koyanagi <yancya@upec.jp>
2021-01-01 15:47:01 +0900

    Add mergin Artwork for ID3 script

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,4 +1,10 @@
 ffmpeg -i ~/Desktop/なんちゃラジオ第156回\ -\ 2021:01:01\ 14.mp3 \
+       -i $(pwd)/../images/artwork.jpg \
+       -map 0 \
+       -map 1 \
+       -c copy \
+       -c:v:1 mjpeg \
+       -id3v2_version 3 \
        -metadata title="第156回「メリークリスマス！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
