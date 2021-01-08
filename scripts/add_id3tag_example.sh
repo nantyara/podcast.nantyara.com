599324e90d05d504ad7adc88bee2e3f34e0161a3
Shinta Koyanagi <yancya@upec.jp>
2021-01-08 13:39:04 +0900

    Add Ep.157

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第156回\ -\ 2021:01:01\ 14.mp3 \
-       -i $(pwd)/../images/artwork.jpg \
+ffmpeg -i ~/Desktop/なんちゃラジオ第157回.mp3 \
+       -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第156回「メリークリスマス！」" \
+       -metadata title="第157回「籤って読めない」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="12月25日の無料ワンマンにて公開録音された回です" \
-       ~/Desktop/out_156.mp3
+       -metadata TIT3="あけましておめで風呂回です" \
+       ~/Desktop/out_157.mp3
