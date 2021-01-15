f392ccb43ccfbe02996245fe63376ee25f33f943
Shinta Koyanagi <yancya@upec.jp>
2021-01-15 19:52:14 +0900

    Add Ep.158

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第157回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第158回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第157回「籤って読めない」" \
+       -metadata title="第158回「なんちゃラジオ３周年」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="あけましておめで風呂回です" \
-       ~/Desktop/out_157.mp3
+       -metadata TIT3="３周年めでたい" \
+       ~/Desktop/158.mp3
