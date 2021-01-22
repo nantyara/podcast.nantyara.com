de5c6721cd8b3f81ae5ee9b3a5ef98e897eeae78
Shinta Koyanagi <yancya@upec.jp>
2021-01-22 22:10:55 +0900

    Add Ep.159

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第158回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第159回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第158回「なんちゃラジオ３周年」" \
+       -metadata title="第159回「現代の海を渡る船」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="３周年めでたい" \
-       ~/Desktop/158.mp3
+       -metadata TIT3="普通に風邪ひきましたね...... 違うんだよ？" \
+       ~/Desktop/159.mp3
