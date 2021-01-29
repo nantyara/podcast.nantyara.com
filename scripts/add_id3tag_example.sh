6f77a60b1a93ebe8c7c5f4ad981a785e8454c1d9
Shinta Koyanagi <yancya@upec.jp>
2021-01-29 23:17:41 +0900

    Add Ep.160

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第159回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第160回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第159回「現代の海を渡る船」" \
+       -metadata title="第160回「広島遠征の夜」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="普通に風邪ひきましたね...... 違うんだよ？" \
-       ~/Desktop/159.mp3
+       -metadata TIT3="広島から２人でお送りします" \
+       ~/Desktop/160.mp3
