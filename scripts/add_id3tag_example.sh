404c130b342d8f483158f57d66be672e8f50538c
Shinta Koyanagi <yancya@upec.jp>
2021-02-05 21:58:43 +0900

    Add Ep.161

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第160回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第161回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第160回「広島遠征の夜」" \
+       -metadata title="第161回「楽器を弾いてみた」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="広島から２人でお送りします" \
-       ~/Desktop/160.mp3
+       -metadata TIT3="楽器を貰いまして、楽器というのは音が出る道具なんですけど" \
+       ~/Desktop/161.mp3
