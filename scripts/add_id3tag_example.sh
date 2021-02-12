2044dedebc9da47bf32164d3ce6c21c136612c7f
Shinta Koyanagi <yancya@upec.jp>
2021-02-13 00:31:05 +0900

    Add Ep.162

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第161回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第162回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第161回「楽器を弾いてみた」" \
+       -metadata title="第162回「生誕について」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="楽器を貰いまして、楽器というのは音が出る道具なんですけど" \
-       ~/Desktop/161.mp3
+       -metadata TIT3="生誕祭 2 DAYS、ありがとうございました" \
+       ~/Desktop/162.mp3
