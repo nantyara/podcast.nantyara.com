02c8cf8c3b2930f172fd1cc3554c903112111aa5
Shinta Koyanagi <yancya@upec.jp>
2021-02-20 00:35:57 +0900

    Add Ep.163

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第162回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第163回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第162回「生誕について」" \
+       -metadata title="第163回「大きな玉ねぎの下で」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="生誕祭 2 DAYS、ありがとうございました" \
-       ~/Desktop/162.mp3
+       -metadata TIT3="色んな事を考えた日でした" \
+       ~/Desktop/163.mp3
