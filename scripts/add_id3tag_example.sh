aa679d32ef6b0242e2552a9e6462d76cf71d8b50
Shinta Koyanagi <yancya@upec.jp>
2021-03-05 22:07:18 +0900

    Add Ep.165

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第164回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第165回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第164回「かちゅぜちゅとれーにんぐ」" \
+       -metadata title="第165回「久々に質問に答えていきます」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ういろううりをご活用ください" \
-       ~/Desktop/164.mp3
+       -metadata TIT3="お風呂で質問に答えます。困った時の原点回帰です" \
+       ~/Desktop/165.mp3
