3eca5737cf70e226eb0f29be078a1d032399f89a
Shinta Koyanagi <yancya@upec.jp>
2021-03-13 16:31:39 +0900

    Add Ep.166

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第165回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第166回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第165回「久々に質問に答えていきます」" \
+       -metadata title="第166回「PUIPUI モルカー」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="お風呂で質問に答えます。困った時の原点回帰です" \
-       ~/Desktop/165.mp3
+       -metadata TIT3="PUIPUI モルカーについて語ります" \
+       ~/Desktop/166.mp3
