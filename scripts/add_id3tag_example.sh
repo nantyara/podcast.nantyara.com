17de65e7d0734e6d37f1fa0f4b4d5f3fdbf0246b
Shinta Koyanagi <yancya@upec.jp>
2021-03-22 13:48:16 +0900

    Add Ep.167

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第166回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第167回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第166回「PUIPUI モルカー」" \
+       -metadata title="第167回「おもちゃキーボードで遊んでみた」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="PUIPUI モルカーについて語ります" \
-       ~/Desktop/166.mp3
+       -metadata TIT3="ミニキーボード UK-01 を買いました" \
+       ~/Desktop/167.mp3
