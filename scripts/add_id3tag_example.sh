1b732478f8ad7aa1207bd687794076973874a8c9
Shinta Koyanagi <yancya@upec.jp>
2021-04-05 01:51:38 +0900

    Add Ep.169

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第168回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第169回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第168回「わけわかんない！」" \
+       -metadata title="第169回「母性！！！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="大阪から、久しぶりに２人で、お送りしています！！！！" \
-       ~/Desktop/168.mp3
+       -metadata TIT3="この声の響き方は、埼玉県！？" \
+       ~/Desktop/169.mp3
