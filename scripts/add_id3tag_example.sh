000b647cc123ae9c474c57ce603c6906a94e7f60
Shinta Koyanagi <yancya@upec.jp>
2021-04-09 12:09:33 +0900

    Add Ep.170

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第169回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第170回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第169回「母性！！！」" \
+       -metadata title="第170回「春は何かを始める季節です」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="この声の響き方は、埼玉県！？" \
-       ~/Desktop/169.mp3
+       -metadata TIT3="みなさんも、何か始めてみては？" \
+       ~/Desktop/170.mp3
