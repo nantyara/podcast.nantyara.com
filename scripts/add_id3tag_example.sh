680ed4c47891cea8133813a52a9d08c4fe8987c2
Shinta Koyanagi <yancya@upec.jp>
2021-04-23 11:41:14 +0900

    Add Ep.172

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第171回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第172回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第171回「みなさ〜ん許してますか！」" \
+       -metadata title="第172回「新曲紹介しちゃおっかな！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="全然大丈夫なので安心して下さい" \
-       ~/Desktop/171.mp3
+       -metadata TIT3="録りたてほやほやをお聴きください" \
+       ~/Desktop/172.mp3
