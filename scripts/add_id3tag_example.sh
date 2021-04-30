f6b89c61c64d97c12fe57836331879a97689212c
Shinta Koyanagi <yancya@upec.jp>
2021-04-30 22:57:43 +0900

    Add Ep.173

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第172回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第173回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第172回「新曲紹介しちゃおっかな！」" \
+       -metadata title="第173回「動画撮影裏話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="録りたてほやほやをお聴きください" \
-       ~/Desktop/172.mp3
+       -metadata TIT3="らーい" \
+       ~/Desktop/173.mp3
