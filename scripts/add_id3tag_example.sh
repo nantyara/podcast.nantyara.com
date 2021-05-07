009029c3b0de9fd5ed4e316bdc030646af8045a5
Shinta Koyanagi <yancya@upec.jp>
2021-05-07 22:04:50 +0900

    Add Ep.174

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第173回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第174回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第173回「動画撮影裏話」" \
+       -metadata title="第174回「幸せってなんだろ？幸せってなあに？って曲あったよなぁ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="らーい" \
-       ~/Desktop/173.mp3
+       -metadata TIT3="ゴールデンウィーク、終わりましたか？" \
+       ~/Desktop/174.mp3
