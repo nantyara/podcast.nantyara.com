029e88069c5785ff504df5da14cde12870c499b6
Shinta Koyanagi <yancya@upec.jp>
2021-05-21 16:13:11 +0900

    Fix title for Ep.176

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第175回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第176回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第175回「ラジオ第175回_母の日企画！心理テストやってみた！」" \
+       -metadata title="第176回「パイの実みたいなデニッシュ食べてみた」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="おなじみ、あおはる風呂回です" \
-       ~/Desktop/175.mp3
+       -metadata TIT3="案件ではないです" \
+       ~/Desktop/176.mp3
