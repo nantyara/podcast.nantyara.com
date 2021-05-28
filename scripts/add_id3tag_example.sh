98a8305af0a0d2ff8044aa0ecb4c5ccec0f42200
Shinta Koyanagi <yancya@upec.jp>
2021-05-28 23:06:20 +0900

    Fix title for Ep.177

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第176回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第177回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第176回「パイの実みたいなデニッシュ食べてみた」" \
+       -metadata title="第177回「ぬるだ〜！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="案件ではないです" \
-       ~/Desktop/176.mp3
+       -metadata TIT3="ぬるというのは、なんちゃらアイドルの割と初期の元メンバーです" \
+       ~/Desktop/177.mp3
