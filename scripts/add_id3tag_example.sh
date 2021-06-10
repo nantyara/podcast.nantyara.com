11779739c279af16097a1feb94953fc971512c06
Shinta Koyanagi <yancya@upec.jp>
2021-06-10 22:30:29 +0900

    Add Ep.179

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第178回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第179回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第177回「おいしい低糖質ピザもどきを作ってみた」" \
+       -metadata title="第179回「雑談回です！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="おいしかったけど、サイリウムのトロトロ感が割と強めなので、パンイメージしてると微妙かも" \
-       ~/Desktop/178.mp3
+       -metadata TIT3="すごく雑談をね、してみちゃおっかな" \
+       ~/Desktop/179.mp3
