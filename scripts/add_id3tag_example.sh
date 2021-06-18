075b8dd6d2a8f1e784de0e95df4fe75323dc0104
Shinta Koyanagi <yancya@upec.jp>
2021-06-19 00:46:19 +0900

    Add Ep.180

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第179回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第180回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第179回「雑談回です！」" \
+       -metadata title="第179回「オトナ帝国の逆襲を思い出す街に行きました」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
        -metadata TIT3="すごく雑談をね、してみちゃおっかな" \
-       ~/Desktop/179.mp3
+       ~/Desktop/180.mp3
