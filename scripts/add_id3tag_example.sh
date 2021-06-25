669e3d068bd47d6a0cad9a79a37fd6fef26f8676
Shinta Koyanagi <yancya@upec.jp>
2021-06-25 17:42:58 +0900

    Add Ep.181

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第180回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第181回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第179回「オトナ帝国の逆襲を思い出す街に行きました」" \
+       -metadata title="第181回「ライブまでの流れ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="すごく雑談をね、してみちゃおっかな" \
-       ~/Desktop/180.mp3
+       -metadata TIT3="8 days ライブをやってます" \
+       ~/Desktop/181.mp3
