1f6ac539c08420db9f970b43df6654eb551934df
Shinta Koyanagi <yancya@upec.jp>
2021-07-03 12:38:54 +0900

    Add Ep.182

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第181回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第182回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第181回「ライブまでの流れ」" \
+       -metadata title="第182回「８デイズについて」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="8 days ライブをやってます" \
-       ~/Desktop/181.mp3
+       -metadata TIT3="８デイズ、終わりました。わ〜" \
+       ~/Desktop/182.mp3
