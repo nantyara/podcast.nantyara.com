245eb15ee5be6b2556baf78ba94ae34724ca096d
Shinta Koyanagi <yancya@upec.jp>
2021-07-10 01:15:00 +0900

    Add Ep.183

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第182回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第183回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第182回「８デイズについて」" \
+       -metadata title="第183回「改名します（やめました）」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="８デイズ、終わりました。わ〜" \
-       ~/Desktop/182.mp3
+       -metadata TIT3="恒例、あおはるちゃんの、風呂ラジ" \
+       ~/Desktop/183.mp3
