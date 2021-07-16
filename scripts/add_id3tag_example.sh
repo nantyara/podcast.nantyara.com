820900434128132fdbcc03afbeed459943604d4b
Shinta Koyanagi <yancya@upec.jp>
2021-07-16 23:05:43 +0900

    Add Ep.184

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第183回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第184回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第183回「改名します（やめました）」" \
+       -metadata title="第184回「モリのアサガオ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="恒例、あおはるちゃんの、風呂ラジ" \
-       ~/Desktop/183.mp3
+       -metadata TIT3="今回は、死刑について考えていきたいと思います" \
+       ~/Desktop/184.mp3
