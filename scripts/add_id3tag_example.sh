d3d653daddf44712be163ae69a630fb37298866a
Shinta Koyanagi <yancya@upec.jp>
2021-07-24 00:58:16 +0900

    Add Ep.185

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第184回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第185回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第184回「モリのアサガオ」" \
+       -metadata title="第185回「秋田遠征」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="今回は、死刑について考えていきたいと思います" \
-       ~/Desktop/184.mp3
+       -metadata TIT3="あおはる風呂ラジオ、秋田遠征についてです" \
+       ~/Desktop/185.mp3
