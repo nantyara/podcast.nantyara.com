2b33a1391ad07d5a88f52cacc222c240e94b438f
Shinta Koyanagi <yancya@upec.jp>
2021-07-24 22:32:57 +0900

    Fix typo

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -5,9 +5,9 @@ ffmpeg -i ~/Desktop/なんちゃラジオ第185回.mp3 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第185回「秋田遠征」" \
+       -metadata title="第185回「東北遠征」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="あおはる風呂ラジオ、秋田遠征についてです" \
+       -metadata TIT3="あおはる風呂ラジオ、東北遠征についてです" \
        ~/Desktop/185.mp3
