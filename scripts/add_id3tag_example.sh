475ba9fb43e1a759919f3cdfea3127bd6e9784bd
Shinta Koyanagi <yancya@upec.jp>
2021-07-31 14:44:02 +0900

    Add Ep.186

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第185回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第186回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第185回「東北遠征」" \
+       -metadata title="第185回「東北はアツかった」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="あおはる風呂ラジオ、東北遠征についてです" \
-       ~/Desktop/185.mp3
+       -metadata TIT3="こ、これが脱水症状〜" \
+       ~/Desktop/186.mp3
