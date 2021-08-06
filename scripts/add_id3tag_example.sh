4bd066ce73bdae922b9e5baeab4f3e375c32db06
Shinta Koyanagi <yancya@upec.jp>
2021-08-06 11:54:16 +0900

    Add Ep.187

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第186回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第187回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第185回「東北はアツかった」" \
+       -metadata title="第187回「なんでもないような事が幸せだったと思う」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="こ、これが脱水症状〜" \
-       ~/Desktop/186.mp3
+       -metadata TIT3="本日も、ご聴、ありがとうございます" \
+       ~/Desktop/187.mp3
