5f3650e8b5d6ae1f4c45978590057eca4fe65878
Shinta Koyanagi <yancya@upec.jp>
2021-08-13 23:26:28 +0900

    Add Ep.188

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第187回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第188回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第187回「なんでもないような事が幸せだったと思う」" \
+       -metadata title="第188回「世界の解像度をあげる」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="本日も、ご聴、ありがとうございます" \
-       ~/Desktop/187.mp3
+       -metadata TIT3="普段考えてることを話します" \
+       ~/Desktop/188.mp3
