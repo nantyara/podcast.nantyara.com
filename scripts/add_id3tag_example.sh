7bd34f8cacf8feed1b5d34ac6f046598b30b93d6
Shinta Koyanagi <yancya@upec.jp>
2021-08-21 01:20:25 +0900

    Add Ep.189

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第188回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第189回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第188回「世界の解像度をあげる」" \
+       -metadata title="第189回「エアコンの掃除をしました」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="普段考えてることを話します" \
-       ~/Desktop/188.mp3
+       -metadata TIT3="体力を付ける運動をしているんですよ" \
+       ~/Desktop/189.mp3
