224f5cc5a6cf8ffd4ba1aa47cc30587ce6aa593c
Shinta Koyanagi <yancya@upec.jp>
2021-08-27 23:05:55 +0900

    Add Ep.190

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第189回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第190回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第189回「エアコンの掃除をしました」" \
+       -metadata title="第190回「なくなった店の名前をぼくたちはもう知らない」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="体力を付ける運動をしているんですよ" \
-       ~/Desktop/189.mp3
+       -metadata TIT3="『残像に口紅を』について話します" \
+       ~/Desktop/190.mp3
