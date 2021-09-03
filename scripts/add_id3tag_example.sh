d8765c513194af37923ca951d7bc384aa6c0639b
Shinta Koyanagi <yancya@upec.jp>
2021-09-04 00:42:00 +0900

    Add Ep.191

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第190回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第191回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第190回「なくなった店の名前をぼくたちはもう知らない」" \
+       -metadata title="第191回「ドラゴンボール読んでる」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="『残像に口紅を』について話します" \
-       ~/Desktop/190.mp3
+       -metadata TIT3="ロマンティックあげるよ" \
+       ~/Desktop/191.mp3
