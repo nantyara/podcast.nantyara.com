bbdce52c7ac6438667c93221b24f7ff80752a0af
Shinta Koyanagi <yancya@upec.jp>
2021-09-12 18:39:05 +0900

    Add Ep.192

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第191回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第192回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第191回「ドラゴンボール読んでる」" \
+       -metadata title="第192回「リングにお砂糖とスパイスと流血」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ロマンティックあげるよ" \
-       ~/Desktop/191.mp3
+       -metadata TIT3="今回は『赤×ピンク』の話してます" \
+       ~/Desktop/192.mp3
