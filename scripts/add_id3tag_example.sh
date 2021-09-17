f8774d2ec04b0962f8e9c1eab6abcec4132d74e5
Shinta Koyanagi <yancya@upec.jp>
2021-09-17 21:45:03 +0900

    Add Ep.193

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第192回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第193回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第192回「リングにお砂糖とスパイスと流血」" \
+       -metadata title="第193回「さる小」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="今回は『赤×ピンク』の話してます" \
-       ~/Desktop/192.mp3
+       -metadata TIT3="今年もさる小に来ています" \
+       ~/Desktop/193.mp3
