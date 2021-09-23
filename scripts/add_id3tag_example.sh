c23fa0400cc4b6f8a00d1b613be07bbcfc5f4812
Shinta Koyanagi <yancya@upec.jp>
2021-09-23 11:22:53 +0900

    Add Ep.194

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第193回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第194回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第193回「さる小」" \
+       -metadata title="第194回「話すってむじいなーーって話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="今年もさる小に来ています" \
-       ~/Desktop/193.mp3
+       -metadata TIT3="「構成」をしていないんですよねぇ……" \
+       ~/Desktop/194.mp3
