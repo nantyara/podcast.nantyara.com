c1b8754759f7588c3f5def43f417af9c62210bf9
Shinta Koyanagi <yancya@upec.jp>
2021-09-30 23:15:53 +0900

    Add Ep.195

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第194回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第195回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第194回「話すってむじいなーーって話」" \
+       -metadata title="第195回「ととのえよう」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="「構成」をしていないんですよねぇ……" \
-       ~/Desktop/194.mp3
+       -metadata TIT3="サウナのね" \
+       ~/Desktop/195.mp3
