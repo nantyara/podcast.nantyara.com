493d8b83ae2212be4ca60c43945dddfae3110b9c
Shinta Koyanagi <yancya@upec.jp>
2021-10-19 06:45:40 +0900

    Add Ep.197

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第196回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第197回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第196回「あぽかりぷす〜」" \
+       -metadata title="第197回「危ないラジオ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ゾンビとかアポカリプスとか好きなんですよね" \
-       ~/Desktop/196.mp3
+       -metadata TIT3="私にとっても、あなたにとっても" \
+       ~/Desktop/197.mp3
