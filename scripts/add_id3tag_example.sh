d762dc9ff8bbc37f522204ee2d84161c0bb3f9e4
Shinta Koyanagi <yancya@upec.jp>
2021-10-22 23:11:44 +0900

    Add Ep.198

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第197回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第198回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第197回「危ないラジオ」" \
+       -metadata title="第198回「読書の秋だ〜」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="私にとっても、あなたにとっても" \
-       ~/Desktop/197.mp3
+       -metadata TIT3="秋っていうのはサ、ご飯が美味しくなるじゃないですか" \
+       ~/Desktop/198.mp3
