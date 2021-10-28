319fbbf8f333eb2bea5038504559843bcca892e8
Shinta Koyanagi <yancya@upec.jp>
2021-10-28 23:48:54 +0900

    Add Ep.199

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第198回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第199回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第198回「読書の秋だ〜」" \
+       -metadata title="第199回「衣替え雑談」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="秋っていうのはサ、ご飯が美味しくなるじゃないですか" \
-       ~/Desktop/198.mp3
+       -metadata TIT3="寒くなってきたでしょ？だから" \
+       ~/Desktop/199.mp3
