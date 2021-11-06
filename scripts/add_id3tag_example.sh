1a597daaa0307cd916e9901bdd06382c44b777a1
Shinta Koyanagi <yancya@upec.jp>
2021-11-06 09:15:38 +0900

    Add Ep.200

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第199回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第200回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第199回「衣替え雑談」" \
+       -metadata title="第200回「200回」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="寒くなってきたでしょ？だから" \
-       ~/Desktop/199.mp3
+       -metadata TIT3="もう４年ですか〜" \
+       ~/Desktop/200.mp3
