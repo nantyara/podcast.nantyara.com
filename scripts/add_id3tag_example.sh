16d5cf50860fbeedd390ff62d1cf866bc722232b
Shinta Koyanagi <yancya@upec.jp>
2021-11-12 22:36:59 +0900

    Add Ep.201

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第200回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第201回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第200回「200回」" \
+       -metadata title="第201回「スペシャルゲスト回」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="もう４年ですか〜" \
-       ~/Desktop/200.mp3
+       -metadata TIT3="スペシャルゲスト、誰なんだ一体" \
+       ~/Desktop/201.mp3
