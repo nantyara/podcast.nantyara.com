5e3952a3fc7cb14a96951456ae530023080a218e
Shinta Koyanagi <yancya@upec.jp>
2021-11-20 01:47:13 +0900

    Add Ep.202

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第201回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第202回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第201回「スペシャルゲスト回」" \
+       -metadata title="第202回「使ってるスマホアプリ紹介してみる」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="スペシャルゲスト、誰なんだ一体" \
-       ~/Desktop/201.mp3
+       -metadata TIT3="はせ参じた次第でございます" \
+       ~/Desktop/202.mp3
