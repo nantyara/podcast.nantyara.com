d2eb0148fe1caee56f471866ff13b625aadddfc3
Shinta Koyanagi <yancya@upec.jp>
2021-11-27 07:55:39 +0900

    Add Ep.203

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第202回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第203回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第202回「使ってるスマホアプリ紹介してみる」" \
+       -metadata title="第203回「曲がり角のロマンスはあるのか？」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="はせ参じた次第でございます" \
-       ~/Desktop/202.mp3
+       -metadata TIT3="芋けんぴ髪に付いてたよ" \
+       ~/Desktop/203.mp3
