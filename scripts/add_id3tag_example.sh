b8037a6adee02f7983be1d15b28fa5157b8720bf
Shinta Koyanagi <yancya@upec.jp>
2021-12-04 01:30:35 +0900

    Add Ep.204

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第203回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第204回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第203回「曲がり角のロマンスはあるのか？」" \
+       -metadata title="第204回「外に出ること」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="芋けんぴ髪に付いてたよ" \
-       ~/Desktop/203.mp3
+       -metadata TIT3="最近行った旅行の話でも" \
+       ~/Desktop/204.mp3
