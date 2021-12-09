cfd6078c76f76ee449e1c5cef35d23671199d9d1
Shinta Koyanagi <yancya@upec.jp>
2021-12-09 23:40:59 +0900

    Add Ep.205

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第204回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第205回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第204回「外に出ること」" \
+       -metadata title="第205回「副反応の話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="最近行った旅行の話でも" \
-       ~/Desktop/204.mp3
+       -metadata TIT3="スタッフが、めちゃんこいた" \
+       ~/Desktop/205.mp3
