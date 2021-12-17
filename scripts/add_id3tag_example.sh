44a0fe67b10a82cdc876feff2440ffa2cf56a2b7
Shinta Koyanagi <yancya@upec.jp>
2021-12-17 23:18:35 +0900

    Add Ep.206

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第205回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第206回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第205回「副反応の話」" \
+       -metadata title="第206回「1年を振り返りましょう」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="スタッフが、めちゃんこいた" \
-       ~/Desktop/205.mp3
+       -metadata TIT3="１月から順に振り返ります" \
+       ~/Desktop/206.mp3
