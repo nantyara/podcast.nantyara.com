94d5bef124474d4381719868653ef1f3de271369
Shinta Koyanagi <yancya@upec.jp>
2021-12-24 19:13:26 +0900

    Add Ep.207

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第206回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第207回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第206回「1年を振り返りましょう」" \
+       -metadata title="第207回「入浴」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="１月から順に振り返ります" \
-       ~/Desktop/206.mp3
+       -metadata TIT3="ありがたいことに、休みが少ないです" \
+       ~/Desktop/207.mp3
