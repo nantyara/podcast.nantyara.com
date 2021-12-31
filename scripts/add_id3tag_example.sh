9624adf79bc00e06725c4a9ff1614b3f638cf299
Shinta Koyanagi <yancya@upec.jp>
2021-12-31 10:54:23 +0900

    Add Ep.208

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第207回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第208回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第207回「入浴」" \
+       -metadata title="第207回「良いお年を」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ありがたいことに、休みが少ないです" \
-       ~/Desktop/207.mp3
+       -metadata TIT3="以上の３つですかね" \
+       ~/Desktop/208.mp3
