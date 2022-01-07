c52842e0fb35c0360aeecf7672ba7b15bdade023
Shinta Koyanagi <yancya@upec.jp>
2022-01-07 23:22:37 +0900

    Add Ep.209

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第208回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第209回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第207回「良いお年を」" \
+       -metadata title="第209回「年越しラジオ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="以上の３つですかね" \
-       ~/Desktop/208.mp3
+       -metadata TIT3="2022年あけましておめでとうございます" \
+       ~/Desktop/209.mp3
