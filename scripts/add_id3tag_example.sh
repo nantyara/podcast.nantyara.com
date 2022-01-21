51110f65ddb6c0b527b2064fa91c579a13c419b6
Shinta Koyanagi <yancya@upec.jp>
2022-01-21 23:20:16 +0900

    Add Ep.211

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第210回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第211回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第210回「あけおめてから2週間」" \
+       -metadata title="第211回「新年初ソロラジオ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ラママ 4 days 終わりました〜" \
-       ~/Desktop/210.mp3
+       -metadata TIT3="色々あったんでね" \
+       ~/Desktop/211.mp3
