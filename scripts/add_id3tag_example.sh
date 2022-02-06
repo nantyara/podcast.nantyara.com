63f6eca9e946802e238d9310936f8b1fb21c3274
Shinta Koyanagi <yancya@upec.jp>
2022-02-06 10:22:47 +0900

    Add Ep.213

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第212回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第213回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第212回「怪物と植物は土から生まれる…ってこと！？」" \
+       -metadata title="第213回「数字の話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="超絶カッコイイジングルになりましたね" \
-       ~/Desktop/212.mp3
+       -metadata TIT3="２５歳に" \
+       ~/Desktop/213.mp3
