02f7115eb07caaffe10fdbf2775eb58f3a32bc58
Shinta Koyanagi <yancya@upec.jp>
2022-02-12 01:21:08 +0900

    Add Ep.214

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第213回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第214回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第213回「数字の話」" \
+       -metadata title="第214回「生誕ライブありがとうございました！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="２５歳に" \
-       ~/Desktop/213.mp3
+       -metadata TIT3="ありがとうございました〜" \
+       ~/Desktop/214.mp3
