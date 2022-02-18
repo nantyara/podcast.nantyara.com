66fafbca143430b801bbe700d685e07ac2d18ce2
Shinta Koyanagi <yancya@upec.jp>
2022-02-19 01:42:29 +0900

    Add Ep.215

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第214回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第215回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第214回「生誕ライブありがとうございました！」" \
+       -metadata title="第215回「ピアノ買った！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ありがとうございました〜" \
-       ~/Desktop/214.mp3
+       -metadata TIT3="すぐに本題に入らせて頂きます" \
+       ~/Desktop/215.mp3
