7b92ad6aade8ef71aa27ce8fdb8868024a46dafb
Shinta Koyanagi <yancya@upec.jp>
2022-02-25 12:36:53 +0900

    Add Ep.216

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第215回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第216回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第215回「ピアノ買った！」" \
+       -metadata title="第216回「ドライヤーの風圧にびっくりする」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="すぐに本題に入らせて頂きます" \
-       ~/Desktop/215.mp3
+       -metadata TIT3="ドライヤーって知ってますか？" \
+       ~/Desktop/216.mp3
