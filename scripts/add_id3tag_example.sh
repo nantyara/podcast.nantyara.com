e4474d85718b66c83a238f8e33bd59d16e68b383
Shinta Koyanagi <yancya@upec.jp>
2022-03-04 23:11:35 +0900

    Add Ep.217

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第216回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第217回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第216回「ドライヤーの風圧にびっくりする」" \
+       -metadata title="第217回「意識向上計画」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ドライヤーって知ってますか？" \
-       ~/Desktop/216.mp3
+       -metadata TIT3="あおはる風呂ラジオです" \
+       ~/Desktop/217.mp3
