1e4a797cb865d4e19e20454b2abe324369ccf891
Shinta Koyanagi <yancya@upec.jp>
2022-03-11 23:36:33 +0900

    Add Ep.218

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第217回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第218回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第217回「意識向上計画」" \
+       -metadata title="第218回「振ったコーラをエンジンにして」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="あおはる風呂ラジオです" \
-       ~/Desktop/217.mp3
+       -metadata TIT3="また冬が" \
+       ~/Desktop/218.mp3
