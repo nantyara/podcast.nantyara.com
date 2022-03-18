d338f10990048d0eb534468107ad18c84a4397ec
Shinta Koyanagi <yancya@upec.jp>
2022-03-18 23:50:31 +0900

    Add Ep.219

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第218回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第219回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第218回「振ったコーラをエンジンにして」" \
+       -metadata title="第219回「料理ってむじーーよな」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="また冬が" \
-       ~/Desktop/218.mp3
+       -metadata TIT3="オリジン弁当もコンビニフードも食べ尽くした" \
+       ~/Desktop/219.mp3
