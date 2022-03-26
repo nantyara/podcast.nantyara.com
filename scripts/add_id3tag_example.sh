9212a7efee3c3e41dba4f5c904da96f2f4561e0e
Shinta Koyanagi <yancya@upec.jp>
2022-03-26 09:39:22 +0900

    Add Ep.220

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第219回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第220回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第219回「料理ってむじーーよな」" \
+       -metadata title="第220回「yo-yo」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="オリジン弁当もコンビニフードも食べ尽くした" \
-       ~/Desktop/219.mp3
+       -metadata TIT3="あくびをお届けします" \
+       ~/Desktop/220.mp3
