43389b2c49aaeec5487cd1d31b3ad7790278cd6c
Shinta Koyanagi <yancya@upec.jp>
2022-04-02 00:04:30 +0900

    Add Ep.221

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第220回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第221回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第220回「yo-yo」" \
+       -metadata title="第221回「音楽の話！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="あくびをお届けします" \
-       ~/Desktop/220.mp3
+       -metadata TIT3="４月ですね。風呂です。あおはるです" \
+       ~/Desktop/221.mp3
