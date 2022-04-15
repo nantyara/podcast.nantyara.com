f89588cdd9901e2a80f3adacd0ff4a707aad63e2
Shinta Koyanagi <yancya@upec.jp>
2022-04-15 23:01:47 +0900

    Add Ep.223

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第222回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第223回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第222回「部屋を片付けたら起きれるようになった話」" \
+       -metadata title="第223回「贈り物の話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ようやっと髪を染めまして" \
-       ~/Desktop/222.mp3
+       -metadata TIT3="あたたかいです" \
+       ~/Desktop/223.mp3
