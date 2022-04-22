4c89555cd67435a3e16bd7271cce387220f36a97
Shinta Koyanagi <yancya@upec.jp>
2022-04-23 00:25:41 +0900

    Add Ep.224

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第223回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第224回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第223回「贈り物の話」" \
+       -metadata title="第224回「名古屋大阪いったってわけ！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="あたたかいです" \
-       ~/Desktop/223.mp3
+       -metadata TIT3="久しぶりに運転しました。怖かった" \
+       ~/Desktop/224.mp3
