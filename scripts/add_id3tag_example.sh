5bfaafc7d2aaaeb46a1d9343071198cadabba385
Shinta Koyanagi <yancya@upec.jp>
2022-05-07 11:48:45 +0900

    Add Ep.226

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第225回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第226回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第225回「催眠音声」" \
+       -metadata title="第226回「北海道はでっかい！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="新しい録音アプリの性能やいかに" \
-       ~/Desktop/225.mp3
+       -metadata TIT3="また北海道行きたい" \
+       ~/Desktop/226.mp3
