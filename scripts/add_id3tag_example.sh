a02eaac307b130d35ca3a7d373da5c4460ec3321
Shinta Koyanagi <yancya@upec.jp>
2022-05-13 22:51:28 +0900

    Add Ep.227

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第226回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第227回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第226回「北海道はでっかい！」" \
+       -metadata title="第227回「ラジオの企画をやってみる」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="また北海道行きたい" \
-       ~/Desktop/226.mp3
+       -metadata TIT3="みんなラジオで何を話してるんですかね" \
+       ~/Desktop/227.mp3
