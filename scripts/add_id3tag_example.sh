3d7124e049274cd4bc298861b7c0c3b272904c8c
Shinta Koyanagi <yancya@upec.jp>
2022-05-27 23:14:08 +0900

    Add Ep.229

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第228回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第229回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第228回「東北ツアーに行ってきたよ！」" \
+       -metadata title="第229回「ココ最近の遠征の思い出」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="海に来ていません" \
-       ~/Desktop/228.mp3
+       -metadata TIT3="真面目に聴いていますか" \
+       ~/Desktop/229.mp3
