1fc78ba6c5013cbae6cbdfa811835e684806f4e4
Shinta Koyanagi <yancya@upec.jp>
2022-06-04 00:35:43 +0900

    Add Ep.230

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第229回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第230回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第229回「ココ最近の遠征の思い出」" \
+       -metadata title="第230回「休みの日なにしてる？」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="真面目に聴いていますか" \
-       ~/Desktop/229.mp3
+       -metadata TIT3="休みです" \
+       ~/Desktop/230.mp3
