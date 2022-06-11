d332958c9617bb5ebd56a150faed677ae5076fee
Shinta Koyanagi <yancya@upec.jp>
2022-06-11 09:53:37 +0900

    Add Ep.231

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第230回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第231回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第230回「休みの日なにしてる？」" \
+       -metadata title="第231回「文化的な生活である必要性無し」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="休みです" \
-       ~/Desktop/230.mp3
+       -metadata TIT3="もしかしたらお金払ってんのかもしれないけど" \
+       ~/Desktop/231.mp3
