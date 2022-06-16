1fd1ecfbe181e227e187c6c1f680f3b7eea6c755
Shinta Koyanagi <yancya@upec.jp>
2022-06-16 13:16:16 +0900

    Add Ep.232

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第231回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第232回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第231回「文化的な生活である必要性無し」" \
+       -metadata title="第232回「高ッケェ歯磨き粉を買いました」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="もしかしたらお金払ってんのかもしれないけど" \
-       ~/Desktop/231.mp3
+       -metadata TIT3="75ml で 2,400円するの" \
+       ~/Desktop/232.mp3
