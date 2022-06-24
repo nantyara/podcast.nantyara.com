29725a067f8f628a816a4b39d4ceaff01ca959e1
Shinta Koyanagi <yancya@upec.jp>
2022-06-24 22:58:13 +0900

    Add Ep.233

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第232回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第233回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第232回「高ッケェ歯磨き粉を買いました」" \
+       -metadata title="第233回「ゲームおもろい再録」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="75ml で 2,400円するの" \
-       ~/Desktop/232.mp3
+       -metadata TIT3="７分半くらいね〜" \
+       ~/Desktop/233.mp3
