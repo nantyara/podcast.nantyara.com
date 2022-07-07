19e0141baa4f10b4d55e8f30cb71fda45bf45da0
Shinta Koyanagi <yancya@upec.jp>
2022-07-08 01:20:16 +0900

    Add Ep.235

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第234回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第235回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第234回「あっちいのでこまめに水分補給しようの会」" \
+       -metadata title="第235回「暑さでアホになる」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="こまめってなんだか知ってますか？" \
-       ~/Desktop/234.mp3
+       -metadata TIT3="人 vs 人" \
+       ~/Desktop/235.mp3
