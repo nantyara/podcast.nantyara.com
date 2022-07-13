f30ede91f3db13163037b6fe11c881c2bffe13b4
Shinta Koyanagi <yancya@upec.jp>
2022-07-13 23:55:52 +0900

    Add Ep.236

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第235回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第236回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第235回「暑さでアホになる」" \
+       -metadata title="第236回「ヴァイオレット・エヴァーガーデンを観た話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="人 vs 人" \
-       ~/Desktop/235.mp3
+       -metadata TIT3="お休みなのよ横になっております" \
+       ~/Desktop/236.mp3
