36baec79e1831d03a123eebe5a093ac762237827
Shinta Koyanagi <yancya@upec.jp>
2022-07-22 23:51:39 +0900

    Add Ep.237

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第236回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第237回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第236回「ヴァイオレット・エヴァーガーデンを観た話」" \
+       -metadata title="第237回「ビーチボーイ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="お休みなのよ横になっております" \
-       ~/Desktop/236.mp3
+       -metadata TIT3="開国してくださ〜い" \
+       ~/Desktop/237.mp3
