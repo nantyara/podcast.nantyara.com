a275a5469fd139a565f1a21bed294d6df75e2906
Shinta Koyanagi <yancya@upec.jp>
2022-07-29 23:10:09 +0900

    Add Ep.238

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第237回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第238回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第237回「ビーチボーイ」" \
+       -metadata title="第238回「夢占いをしたらまさかの結果に」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="開国してくださ〜い" \
-       ~/Desktop/237.mp3
+       -metadata TIT3="夢日記をですね" \
+       ~/Desktop/238.mp3
