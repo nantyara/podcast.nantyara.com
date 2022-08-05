f83386e34c0f377a158cd25777d8903389fe2357
Shinta Koyanagi <yancya@upec.jp>
2022-08-05 21:22:39 +0900

    Add Ep.239

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第238回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第239回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第238回「夢占いをしたらまさかの結果に」" \
+       -metadata title="第239回「海に関する思い出」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="夢日記をですね" \
-       ~/Desktop/238.mp3
+       -metadata TIT3="海に行く予定があるんですよ" \
+       ~/Desktop/239.mp3
