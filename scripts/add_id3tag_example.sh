b109eff04f495735e7d2ea9c1a3b7fa9532746f2
Shinta Koyanagi <yancya@upec.jp>
2022-08-14 11:36:25 +0900

    Add Ep.240

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第239回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第240回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第239回「海に関する思い出」" \
+       -metadata title="第240「リメンバーミー」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="海に行く予定があるんですよ" \
-       ~/Desktop/239.mp3
+       -metadata TIT3="セミっているじゃないですか" \
+       ~/Desktop/240.mp3
