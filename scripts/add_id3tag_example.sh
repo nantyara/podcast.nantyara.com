f0c2f7220fc30dd34596937fa74cf63b9c2b2409
Shinta Koyanagi <yancya@upec.jp>
2022-08-26 21:38:51 +0900

    Add Ep.242

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第241回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第242回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第241回「動物占いたぬき」" \
+       -metadata title="第242回「虫まじ無理だしって話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="なななななななないから" \
-       ~/Desktop/241.mp3
+       -metadata TIT3="夏と言えばお祭とか" \
+       ~/Desktop/242.mp3
