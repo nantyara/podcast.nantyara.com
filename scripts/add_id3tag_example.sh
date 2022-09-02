dfeef2680aaa3dcb2f09e748a6569df611250283
Shinta Koyanagi <yancya@upec.jp>
2022-09-02 23:26:27 +0900

    Add Ep.243

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第242回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第243回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第242回「虫まじ無理だしって話」" \
+       -metadata title="第243回「一皮むけたあおはる」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="夏と言えばお祭とか" \
-       ~/Desktop/242.mp3
+       -metadata TIT3="スマカノ撮影の思い出" \
+       ~/Desktop/243.mp3
