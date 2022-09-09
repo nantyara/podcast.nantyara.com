34bd1f5d1e71001b662442ebe30dd741f0b78ad3
Shinta Koyanagi <yancya@upec.jp>
2022-09-10 01:51:47 +0900

    Add Ep.244

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第243回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第244回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第243回「一皮むけたあおはる」" \
+       -metadata title="第244回「そういや夏って怪談もあったね」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="スマカノ撮影の思い出" \
-       ~/Desktop/243.mp3
+       -metadata TIT3="夏と言えばなにかしら" \
+       ~/Desktop/244.mp3
