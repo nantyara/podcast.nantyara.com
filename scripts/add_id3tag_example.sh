69b096d7b072ed98eb4946ed4d56e873bd55d186
Shinta Koyanagi <yancya@upec.jp>
2022-09-17 17:38:52 +0900

    Add Ep.245

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第244回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第245回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第244回「そういや夏って怪談もあったね」" \
+       -metadata title="第245回「好きなコンテンツ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="夏と言えばなにかしら" \
-       ~/Desktop/244.mp3
+       -metadata TIT3="宵越しの銭は持たぬように" \
+       ~/Desktop/245.mp3
