80532f9c43337e935882953e1085faf3cdf349d9
Shinta Koyanagi <yancya@upec.jp>
2022-09-23 23:22:29 +0900

    Add Ep.246

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第245回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第246回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第245回「好きなコンテンツ」" \
+       -metadata title="第246回「また虫と怖い話してます」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="宵越しの銭は持たぬように" \
-       ~/Desktop/245.mp3
+       -metadata TIT3="解決策からお話しします" \
+       ~/Desktop/246.mp3
