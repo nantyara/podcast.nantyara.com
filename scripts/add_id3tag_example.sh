5d02ea860a9c251bbe4e069d4861207824ef2b5a
Shinta Koyanagi <yancya@upec.jp>
2022-10-01 01:23:35 +0900

    Add Ep.247

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第246回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第247回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第246回「また虫と怖い話してます」" \
+       -metadata title="第247回「福岡行ってきた」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="解決策からお話しします" \
-       ~/Desktop/246.mp3
+       -metadata TIT3="話したいことがたくさんある" \
+       ~/Desktop/247.mp3
