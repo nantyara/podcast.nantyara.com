4a451adebdf66f4c24446dd0b972a39267454375
Shinta Koyanagi <yancya@upec.jp>
2022-10-08 12:21:31 +0900

    Add Ep.248

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第247回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第248回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第247回「福岡行ってきた」" \
+       -metadata title="第248回「太宰府いったから受験しようか悩んでます」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="話したいことがたくさんある" \
-       ~/Desktop/247.mp3
+       -metadata TIT3="秋めいてきましたね" \
+       ~/Desktop/248.mp3
