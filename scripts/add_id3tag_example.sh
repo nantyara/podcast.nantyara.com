c6c08a658b4842efb0681003dc829d885d1ae39e
Shinta Koyanagi <yancya@upec.jp>
2022-10-14 23:01:15 +0900

    Add Ep.249

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第248回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第249回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第248回「太宰府いったから受験しようか悩んでます」" \
+       -metadata title="第249回「なんちゃら、バンド辞めるってよ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="秋めいてきましたね" \
-       ~/Desktop/248.mp3
+       -metadata TIT3="awsome" \
+       ~/Desktop/249.mp3
