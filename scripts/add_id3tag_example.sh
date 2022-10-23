b64b96a413935010753e8dd2709a34186e147a43
Shinta Koyanagi <yancya@upec.jp>
2022-10-23 21:44:23 +0900

    Add Ep.250

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第249回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第250回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第249回「なんちゃら、バンド辞めるってよ」" \
+       -metadata title="第250回「スポーツの秋と心の筋肉痛」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="awsome" \
-       ~/Desktop/249.mp3
+       -metadata TIT3="秋めいてまいりましたね" \
+       ~/Desktop/250.mp3
