748d699b65e1531a749ec42b64fe22ed99747341
Shinta Koyanagi <yancya@upec.jp>
2022-10-29 02:59:27 +0900

    Add Ep.251

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第250回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第251回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第250回「スポーツの秋と心の筋肉痛」" \
+       -metadata title="第251回「あーとかうーとか言わない」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="秋めいてまいりましたね" \
-       ~/Desktop/250.mp3
+       -metadata TIT3="タイムリミットがあります" \
+       ~/Desktop/251.mp3
