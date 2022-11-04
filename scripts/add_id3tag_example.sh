2d099e0c0246010ff147bbc35bc103266240ac38
Shinta Koyanagi <yancya@upec.jp>
2022-11-04 16:54:42 +0900

    Add Ep.252

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第251回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第252回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第251回「あーとかうーとか言わない」" \
+       -metadata title="第252回「女の子ってすげぇなって思った話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="タイムリミットがあります" \
-       ~/Desktop/251.mp3
+       -metadata TIT3="バキバキに噛んでも録り直しません" \
+       ~/Desktop/252.mp3
