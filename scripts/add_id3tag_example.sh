fd0f476594bfd0c1838c179adbbfdc2fc409a432
Shinta Koyanagi <yancya@upec.jp>
2022-11-11 12:04:15 +0900

    Add Ep.253

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第252回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第253回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第252回「女の子ってすげぇなって思った話」" \
+       -metadata title="第253回「主催セブンデイズもうすぐ！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="バキバキに噛んでも録り直しません" \
-       ~/Desktop/252.mp3
+       -metadata TIT3="ここはね、私の部屋だから、好きなことを好きな風に話しますよ。どんなもんじゃい！" \
+       ~/Desktop/253.mp3
