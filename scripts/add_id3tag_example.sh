ec97f6bfaa2e2d4afaf483e21ea1529d4442641c
Shinta Koyanagi <yancya@upec.jp>
2022-11-18 22:25:01 +0900

    Add Ep.254

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第253回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第254回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第253回「主催セブンデイズもうすぐ！」" \
+       -metadata title="第254回「フォースと共にあらんことを」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ここはね、私の部屋だから、好きなことを好きな風に話しますよ。どんなもんじゃい！" \
-       ~/Desktop/253.mp3
+       -metadata TIT3="けのびを思い出しちゃうよね" \
+       ~/Desktop/254.mp3
