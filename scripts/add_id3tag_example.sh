c379b8275715efecc47866291b65a0692ef0879e
Shinta Koyanagi <yancya@upec.jp>
2022-11-25 23:11:16 +0900

    Add Ep.255

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第254回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第255回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第254回「フォースと共にあらんことを」" \
+       -metadata title="第255回「7日間ありがとうございました」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="けのびを思い出しちゃうよね" \
-       ~/Desktop/254.mp3
+       -metadata TIT3="さて、終わりましたね。ありがとうございました。" \
+       ~/Desktop/255.mp3
