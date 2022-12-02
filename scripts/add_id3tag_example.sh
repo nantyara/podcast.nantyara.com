7f1b92596d1dc56e5ae0cfc50735fb516431f205
Shinta Koyanagi <yancya@upec.jp>
2022-12-02 23:15:00 +0900

    Add Ep.256

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第255回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第256回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第255回「7日間ありがとうございました」" \
+       -metadata title="第256回「こういう時はこの曲を」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="さて、終わりましたね。ありがとうございました。" \
-       ~/Desktop/255.mp3
+       -metadata TIT3="なんなんだこの茶番は〜" \
+       ~/Desktop/256.mp3
