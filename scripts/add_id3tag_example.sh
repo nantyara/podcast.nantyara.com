7a80b4847b7a9650f5627697d00634591bbc48ee
Shinta Koyanagi <yancya@upec.jp>
2022-12-08 23:07:29 +0900

    Add Ep.257

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第256回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第257回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第256回「こういう時はこの曲を」" \
+       -metadata title="第257回「みたものよんだもの」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="なんなんだこの茶番は〜" \
-       ~/Desktop/256.mp3
+       -metadata TIT3="ハードをできるだけ下げていこう" \
+       ~/Desktop/257.mp3
