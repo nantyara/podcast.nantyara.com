3d15900b9206af21b703d67123de2630744cff8a
Shinta Koyanagi <yancya@upec.jp>
2022-12-17 11:31:17 +0900

    Add Ep.258

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第257回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第258回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第257回「みたものよんだもの」" \
+       -metadata title="第258回「秋田は寒いがあったけェ場所」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ハードをできるだけ下げていこう" \
-       ~/Desktop/257.mp3
+       -metadata TIT3="新幹線すごい" \
+       ~/Desktop/258.mp3
