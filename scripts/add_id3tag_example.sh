006042912969403526d124c8f26ec775fe1927d3
Shinta Koyanagi <yancya@upec.jp>
2022-12-24 14:08:07 +0900

    Add Ep.259

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第258回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第259回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第258回「秋田は寒いがあったけェ場所」" \
+       -metadata title="第259回「Life goes on全曲レビュー」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="新幹線すごい" \
-       ~/Desktop/258.mp3
+       -metadata TIT3="聞（聴）いてくださいよ皆さん" \
+       ~/Desktop/259.mp3
