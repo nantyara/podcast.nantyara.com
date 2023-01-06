a4dc48183b6c40c02b429cedba3f5b1f54cc821a
Shinta Koyanagi <yancya@upec.jp>
2023-01-06 22:07:49 +0900

    Add Ep.261

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第260回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第261回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第260回「2022が終わるってマジ？」" \
+       -metadata title="第261回「2023年の抱負！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="散弾銃とか" \
-       ~/Desktop/260.mp3
+       -metadata TIT3="だいぶ続いてますね" \
+       ~/Desktop/261.mp3
