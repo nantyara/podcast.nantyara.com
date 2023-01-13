1ee1f58c784f419a68536e7ed5a50b4b4e051c73
Shinta Koyanagi <yancya@upec.jp>
2023-01-13 11:49:47 +0900

    Add Ep.262

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第261回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第262回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第261回「2023年の抱負！」" \
+       -metadata title="第262回「あけましておめでとうござい、まーーーーーーす！！！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="だいぶ続いてますね" \
-       ~/Desktop/261.mp3
+       -metadata TIT3="ドラえもんだったり、ニーチェだったり" \
+       ~/Desktop/262.mp3
