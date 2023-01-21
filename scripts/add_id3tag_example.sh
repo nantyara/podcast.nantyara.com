b26ab3e11ec99001084591681c78f8c1a016480a
Shinta Koyanagi <yancya@upec.jp>
2023-01-21 09:12:34 +0900

    Add Ep.263

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第262回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第263回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第262回「あけましておめでとうござい、まーーーーーーす！！！」" \
+       -metadata title="第263回「なんちゃラジオ5周年おめでとう」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ドラえもんだったり、ニーチェだったり" \
-       ~/Desktop/262.mp3
+       -metadata TIT3="1月29日が誕生日なんですよ〜" \
+       ~/Desktop/263.mp3
