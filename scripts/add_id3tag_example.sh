d3076fdc814a3d3db463019a25064f47e8316c5a
Shinta Koyanagi <yancya@upec.jp>
2023-01-28 02:08:44 +0900

    Add Ep.264

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第263回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第264回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第263回「なんちゃラジオ5周年おめでとう」" \
+       -metadata title="第264回「リッキッシーッ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="1月29日が誕生日なんですよ〜" \
-       ~/Desktop/263.mp3
+       -metadata TIT3="大型深海魚の新種「ヨコヅナイワシ」を駿河湾で発見" \
+       ~/Desktop/264.mp3
