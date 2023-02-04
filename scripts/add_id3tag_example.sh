f67dd20a2166905fc1ece0764852364d3fe77c60
Shinta Koyanagi <yancya@upec.jp>
2023-02-04 10:25:14 +0900

    Add Ep.265

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第264回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第265回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第264回「リッキッシーッ」" \
+       -metadata title="第265回「風呂の温度」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="大型深海魚の新種「ヨコヅナイワシ」を駿河湾で発見" \
-       ~/Desktop/264.mp3
+       -metadata TIT3="旅に出ます" \
+       ~/Desktop/265.mp3
