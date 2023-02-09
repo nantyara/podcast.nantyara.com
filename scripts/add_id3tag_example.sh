e69a2df6ae73188e3300d90cb61f251f72ff0ab7
Shinta Koyanagi <yancya@upec.jp>
2023-02-09 11:29:07 +0900

    Add Ep.266

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第265回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第266回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第265回「風呂の温度」" \
+       -metadata title="第266回「10年後も同じ歌君に歌いたい」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="旅に出ます" \
-       ~/Desktop/265.mp3
+       -metadata TIT3="生誕ライブありがとうございました" \
+       ~/Desktop/266.mp3
