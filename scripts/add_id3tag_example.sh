fef4ace8595377038f1aeac11b28bea2865efe79
Shinta Koyanagi <yancya@upec.jp>
2023-02-16 10:35:29 +0900

    Add Ep.267

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第266回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第267回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第266回「10年後も同じ歌君に歌いたい」" \
+       -metadata title="第267回「山やべえ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="生誕ライブありがとうございました" \
-       ~/Desktop/266.mp3
+       -metadata TIT3="なんちゃらやまんにゅ" \
+       ~/Desktop/267.mp3
