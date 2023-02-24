abe111bac2e46ff0be71c3d49c2ee09a86461e98
Shinta Koyanagi <yancya@upec.jp>
2023-02-24 22:17:10 +0900

    Add Ep.268

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第267回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第268回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第267回「山やべえ」" \
+       -metadata title="第268回「好き勝手に話して」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="なんちゃらやまんにゅ" \
-       ~/Desktop/267.mp3
+       -metadata TIT3="生誕monthも山も終わり" \
+       ~/Desktop/268.mp3
