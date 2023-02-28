ca3a1f02fab234f7f6776b5b61adfae1c3ab7ee2
Shinta Koyanagi <yancya@upec.jp>
2023-03-01 08:49:57 +0900

    Add Ep.269

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第268回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第269回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第268回「好き勝手に話して」" \
+       -metadata title="第269回「旅ログ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="生誕monthも山も終わり" \
-       ~/Desktop/268.mp3
+       -metadata TIT3="ベトナムのホーチミンに行ってまいりました" \
+       ~/Desktop/269.mp3
