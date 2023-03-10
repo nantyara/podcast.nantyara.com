3fc341087636ff306544b5bf48ca5d8a5f4e5c4d
Shinta Koyanagi <yancya@upec.jp>
2023-03-10 23:55:22 +0900

    Add Ep.270

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第269回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第270回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第269回「旅ログ」" \
+       -metadata title="第270回「5周年だったの知らなかったってば！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ベトナムのホーチミンに行ってまいりました" \
-       ~/Desktop/269.mp3
+       -metadata TIT3="最近聴き始めたって人もいて、嬉しいですね" \
+       ~/Desktop/270.mp3
