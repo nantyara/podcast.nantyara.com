b2559393625db46dfe1bb1de19a818e1b8f6440e
Shinta Koyanagi <yancya@upec.jp>
2023-03-17 22:51:20 +0900

    Add Ep.271

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第270回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第271回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第270回「5周年だったの知らなかったってば！」" \
+       -metadata title="第271回「食べたこと無いものの話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="最近聴き始めたって人もいて、嬉しいですね" \
-       ~/Desktop/270.mp3
+       -metadata TIT3="花粉がねぇ〜" \
+       ~/Desktop/271.mp3
