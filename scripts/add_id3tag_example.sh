6ee58240bc1ef643bf9b59a96e8fc8175e91f26a
Shinta Koyanagi <yancya@upec.jp>
2023-03-25 00:25:37 +0900

    Add Ep.272

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第271回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第272回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第271回「食べたこと無いものの話」" \
+       -metadata title="第272回「ワクワクワク休暇」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="花粉がねぇ〜" \
-       ~/Desktop/271.mp3
+       -metadata TIT3="大人だから、泣かないじゃないですか" \
+       ~/Desktop/272.mp3
