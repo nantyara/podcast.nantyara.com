b645b539402b50ecba32336e377dbc2e99e933a9
Shinta Koyanagi <yancya@upec.jp>
2023-03-31 23:23:20 +0900

    Add Ep.273

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第272回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第273回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第272回「ワクワクワク休暇」" \
+       -metadata title="第273回「おはなみ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="大人だから、泣かないじゃないですか" \
-       ~/Desktop/272.mp3
+       -metadata TIT3="珍しい外回です" \
+       ~/Desktop/273.mp3
