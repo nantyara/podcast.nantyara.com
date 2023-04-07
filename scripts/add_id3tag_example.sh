226d192dc6a5bc89de180038190be14ffe539159
Shinta Koyanagi <yancya@upec.jp>
2023-04-07 22:59:02 +0900

    Add Ep.274

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第273回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第274回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第273回「おはなみ」" \
+       -metadata title="第274回「ご指導ご鞭撻」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="珍しい外回です" \
-       ~/Desktop/273.mp3
+       -metadata TIT3="新年度っていう単語が出てこなかった" \
+       ~/Desktop/274.mp3
