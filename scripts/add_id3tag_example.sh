26579d673bb8cac637171161fd5cf3bb0af649f2
Shinta Koyanagi <yancya@upec.jp>
2023-04-15 00:04:00 +0900

    Add Ep.275

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第274回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第275回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第274回「ご指導ご鞭撻」" \
+       -metadata title="第275回「薬局としての日高屋」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="新年度っていう単語が出てこなかった" \
-       ~/Desktop/274.mp3
+       -metadata TIT3="チェーン店ってどうですか？" \
+       ~/Desktop/275.mp3
