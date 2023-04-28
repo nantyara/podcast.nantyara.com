2e47b0b8a0a70800fa9d890ec128423c7a701fef
Shinta Koyanagi <yancya@upec.jp>
2023-04-28 21:34:08 +0900

    Add Ep.277

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第276回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第277回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第276回「MV撮影したよ！！！」" \
+       -metadata title="第277回「いっぱいやすんだ！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="いちばん理想的なクラウドファンディングとは" \
-       ~/Desktop/276.mp3
+       -metadata TIT3="９日間もライブが無かったですか" \
+       ~/Desktop/277.mp3
