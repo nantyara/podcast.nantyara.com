58c715cd62e9ec2ba51fceed05fc54daf3e09a40
Shinta Koyanagi <yancya@upec.jp>
2023-05-05 23:09:58 +0900

    Add Ep.278

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第277回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第278回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第277回「いっぱいやすんだ！」" \
+       -metadata title="第278回「山と懐かしの音楽と」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="９日間もライブが無かったですか" \
-       ~/Desktop/277.mp3
+       -metadata TIT3="GWも終盤に差し掛かってきました" \
+       ~/Desktop/278.mp3
