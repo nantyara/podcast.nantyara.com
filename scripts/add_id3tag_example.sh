c2c109118cb074c8f9b9e6577e281614ae056d9c
Shinta Koyanagi <yancya@upec.jp>
2023-05-20 10:55:53 +0900

    Add Ep.280

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第279回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第280回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第279回「GW！！！！！」" \
+       -metadata title="第280回「大阪は食い倒れ、京都は着倒れ、では奈良は？」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="へいへいへいへいゴールデンウィークが終わりましたへい" \
-       ~/Desktop/279.mp3
+       -metadata TIT3="関西遠征に行ってきまっす" \
+       ~/Desktop/280.mp3
