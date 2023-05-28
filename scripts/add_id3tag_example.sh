835d1e8e38848f7e3f5dbf95ba8d866557101d92
Shinta Koyanagi <yancya@upec.jp>
2023-05-28 11:14:55 +0900

    Add Ep.281

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第280回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第281回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第280回「大阪は食い倒れ、京都は着倒れ、では奈良は？」" \
+       -metadata title="第281回「U☆S☆J」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="関西遠征に行ってきまっす" \
-       ~/Desktop/280.mp3
+       -metadata TIT3="久しぶりの遠征ラジオ〜" \
+       ~/Desktop/281.mp3
