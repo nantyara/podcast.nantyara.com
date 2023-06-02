ca7f92d58e919cced1ba6f28fc95e610f610fa74
Shinta Koyanagi <yancya@upec.jp>
2023-06-03 07:26:14 +0900

    Add Ep.282

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第281回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第282回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第281回「U☆S☆J」" \
+       -metadata title="第282回「ママはる友達多い」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="久しぶりの遠征ラジオ〜" \
-       ~/Desktop/281.mp3
+       -metadata TIT3="スタバに１日に２回も行ったんですよ" \
+       ~/Desktop/282.mp3
