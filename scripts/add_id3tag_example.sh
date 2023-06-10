b772c177513b1c59e336aa1738148d86c06d8537
Shinta Koyanagi <yancya@upec.jp>
2023-06-10 15:23:03 +0900

    Add Ep.283

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第282回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第283回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第282回「ママはる友達多い」" \
+       -metadata title="第283回「推し活」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="スタバに１日に２回も行ったんですよ" \
-       ~/Desktop/282.mp3
+       -metadata TIT3="人生で初めて推しのコラボカフェに" \
+       ~/Desktop/283.mp3
