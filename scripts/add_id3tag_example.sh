d7cb9281d9623ca95f5fd3b1b4ff57f17d9ce138
Shinta Koyanagi <yancya@upec.jp>
2023-06-16 14:49:23 +0900

    Add Ep.284

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第283回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第284回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第283回「推し活」" \
+       -metadata title="第284回「気休めが必要」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="人生で初めて推しのコラボカフェに" \
-       ~/Desktop/283.mp3
+       -metadata TIT3="ソリティアやめるか〜" \
+       ~/Desktop/284.mp3
