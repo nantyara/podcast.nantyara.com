fec45481cc9813b7f42c923fc305527d36c5c9f1
Shinta Koyanagi <yancya@upec.jp>
2023-07-21 23:02:29 +0900

    Add Ep.289

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第288回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第289回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第288回「清潔快適空間」" \
+       -metadata title="第289回「変わること変わらないこと」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="退院しました" \
-       ~/Desktop/288.mp3
+       -metadata TIT3="本当に暑い" \
+       ~/Desktop/289.mp3
