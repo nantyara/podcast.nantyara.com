8f82ed89693d67f155441d2e3b9f9e52cff9f641
Shinta Koyanagi <yancya@upec.jp>
2023-08-05 01:15:58 +0900

    Add Ep.291

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第290回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第291回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第290回「MBTIってなんだ？」" \
+       -metadata title="第291回「最近の出来事など」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="まぁ、性格なんて、こんなもので測れるものではないですし" \
-       ~/Desktop/290.mp3
+       -metadata TIT3="６０歳か〜" \
+       ~/Desktop/291.mp3
