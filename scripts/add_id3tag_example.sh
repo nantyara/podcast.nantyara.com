d8d3eef0c65d33fa445c9602f3c685936a8804b4
Shinta Koyanagi <yancya@upec.jp>
2023-08-12 10:24:53 +0900

    Add Ep.292

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第291回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第292回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第291回「最近の出来事など」" \
+       -metadata title="第292回「ゼルダの伝説面白すぎるンゴ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="６０歳か〜" \
-       ~/Desktop/291.mp3
+       -metadata TIT3="ンゴ" \
+       ~/Desktop/292.mp3
