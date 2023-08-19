99a14ba811ac181f195003efda0d413764163721
Shinta Koyanagi <yancya@upec.jp>
2023-08-19 10:55:07 +0900

    Add Ep.293

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第292回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第293回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第292回「ゼルダの伝説面白すぎるンゴ」" \
+       -metadata title="第293回「私ってやつぁなんでこんなに寝ちゃうんだ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ンゴ" \
-       ~/Desktop/292.mp3
+       -metadata TIT3="人々って休みの日なにやってんだ" \
+       ~/Desktop/293.mp3
