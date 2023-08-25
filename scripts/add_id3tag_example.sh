47adb4152d894e89fb4bfc3f56517f8c2f06c251
Shinta Koyanagi <yancya@upec.jp>
2023-08-26 04:53:41 +0900

    Add Ep.294

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第293回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第294回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第293回「私ってやつぁなんでこんなに寝ちゃうんだ」" \
+       -metadata title="第294回「夏の思い出」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="人々って休みの日なにやってんだ" \
-       ~/Desktop/293.mp3
+       -metadata TIT3="お盆がありましたでしょう？" \
+       ~/Desktop/294.mp3
