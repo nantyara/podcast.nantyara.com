3dc04735a7a277eb845f133a692368a661761a16
Shinta Koyanagi <yancya@upec.jp>
2023-09-04 10:51:57 +0900

    Add Ep.295

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第294回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第295回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第294回「夏の思い出」" \
+       -metadata title="第295回「今から悲しい話をします」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="お盆がありましたでしょう？" \
-       ~/Desktop/294.mp3
+       -metadata TIT3="超最悪です" \
+       ~/Desktop/295.mp3
