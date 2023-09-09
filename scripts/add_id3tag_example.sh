de46cee5f5091d825c24bb8eae50ba0a54064dec
Shinta Koyanagi <yancya@upec.jp>
2023-09-09 13:12:19 +0900

    Add Ep.296

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第295回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第296回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第295回「今から悲しい話をします」" \
+       -metadata title="第296回「遣ってしまうもの、それは」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="超最悪です" \
-       ~/Desktop/295.mp3
+       -metadata TIT3="なすぎてビックリ" \
+       ~/Desktop/296.mp3
