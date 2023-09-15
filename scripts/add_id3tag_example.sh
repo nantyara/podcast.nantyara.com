649053cc16eec549da974f8d7a3c8f7513a617d0
Shinta Koyanagi <yancya@upec.jp>
2023-09-16 03:31:30 +0900

    Add Ep.297

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第296回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第297回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第296回「遣ってしまうもの、それは」" \
+       -metadata title="第297回「さる小」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="なすぎてビックリ" \
-       ~/Desktop/296.mp3
+       -metadata TIT3="さる小の思い出" \
+       ~/Desktop/297.mp3
