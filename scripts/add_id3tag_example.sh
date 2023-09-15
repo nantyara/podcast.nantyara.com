c4379f3a6555b0b89bb3af5137c4f9efb149d21f
Shinta Koyanagi <yancya@upec.jp>
2023-09-16 03:34:52 +0900

    Add Ep.300

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第297回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第300回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第297回「さる小」" \
+       -metadata title="第300回「300回記念回」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="さる小の思い出" \
-       ~/Desktop/297.mp3
+       -metadata TIT3="６年もやってるの" \
+       ~/Desktop/300.mp3
