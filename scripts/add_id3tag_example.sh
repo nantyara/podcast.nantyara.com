855533c3374be05a5938cc3009aec2532ad9d854
Shinta Koyanagi <yancya@upec.jp>
2023-09-25 23:54:42 +0900

    Add Ep.298

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第300回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第298回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第300回「300回記念回」" \
+       -metadata title="第298回「そろそろっとソロの話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="６年もやってるの" \
-       ~/Desktop/300.mp3
+       -metadata TIT3="ソロの" \
+       ~/Desktop/298.mp3
