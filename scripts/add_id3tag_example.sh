5a5500f9b1ee1e8d473d85cf829106ab1cb7a892
Shinta Koyanagi <yancya@upec.jp>
2023-09-30 21:56:14 +0900

    Add Ep.299

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第298回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第299回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第298回「そろそろっとソロの話」" \
+       -metadata title="第299回「食べログ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ソロの" \
-       ~/Desktop/298.mp3
+       -metadata TIT3="1000回、2000回、3000回と目指していきたいですね" \
+       ~/Desktop/299.mp3
