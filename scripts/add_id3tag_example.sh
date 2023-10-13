4c469dc4e69ba0c2127668174caa499604377147
Shinta Koyanagi <yancya@upec.jp>
2023-10-13 16:26:10 +0900

    Add Ep.301

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第299回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第301回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第299回「食べログ」" \
+       -metadata title="第301回「韓国ライブ旅行記」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="1000回、2000回、3000回と目指していきたいですね" \
-       ~/Desktop/299.mp3
+       -metadata TIT3="２人でふりかえる韓国ライブ旅行記" \
+       ~/Desktop/301.mp3
