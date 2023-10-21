58208a42bd5a18efa92dacd4d05e31b42791678d
Shinta Koyanagi <yancya@upec.jp>
2023-10-21 13:48:14 +0900

    Add Ep.302

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第301回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第302回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第301回「韓国ライブ旅行記」" \
+       -metadata title="第302回「帰国後の話（初めて帰国した）」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="２人でふりかえる韓国ライブ旅行記" \
-       ~/Desktop/301.mp3
+       -metadata TIT3="韓国から帰ってきて、色々ライブあった" \
+       ~/Desktop/302.mp3
