246eb044d132de7439d74fc9fc6ac79ccabcb1f8
Shinta Koyanagi <yancya@upec.jp>
2023-10-28 01:14:13 +0900

    Add Ep.303

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第302回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第303回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第302回「帰国後の話（初めて帰国した）」" \
+       -metadata title="第303回「東京タワーにのぼってきたよ！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="韓国から帰ってきて、色々ライブあった" \
-       ~/Desktop/302.mp3
+       -metadata TIT3="なんちゃらアイドルのあおはるがお送りしておりますわ。ってな" \
+       ~/Desktop/303.mp3
