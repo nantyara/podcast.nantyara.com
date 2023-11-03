2f85710f096b977d494d7480de813cbfe1ab7f70
Shinta Koyanagi <yancya@upec.jp>
2023-11-04 01:12:51 +0900

    Add Ep.304

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第303回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第304回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第303回「東京タワーにのぼってきたよ！」" \
+       -metadata title="第304回「栃木の夜」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="なんちゃらアイドルのあおはるがお送りしておりますわ。ってな" \
-       ~/Desktop/303.mp3
+       -metadata TIT3="赤ちゃんにも観てもらって" \
+       ~/Desktop/304.mp3
