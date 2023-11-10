400d894fa3159eeef49df0b92c9d7f1e8552a8f4
Shinta Koyanagi <yancya@upec.jp>
2023-11-11 01:34:59 +0900

    Add Ep.306

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第305回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第306回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第305回「遠征からの遠征」" \
+       -metadata title="第306回「福岡の夜」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="東京タワーって登ったことなくて" \
-        ~/Desktop/305.mp3
+       -metadata TIT3="ぎゅっとね、やってますから" \
+        ~/Desktop/306.mp3
