32b0d9865ec13b38ce7d1c6e93fe306de5fb9b90
Shinta Koyanagi <yancya@upec.jp>
2023-11-11 01:16:23 +0900

    Add Ep.305

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第304回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第305回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第304回「栃木の夜」" \
+       -metadata title="第305回「遠征からの遠征」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="赤ちゃんにも観てもらって" \
-       ~/Desktop/304.mp3
+       -metadata TIT3="東京タワーって登ったことなくて" \
+        ~/Desktop/305.mp3
