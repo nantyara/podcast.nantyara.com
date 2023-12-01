0c2c44e1b0aca16932946177f81f55e92b3882dd
Shinta Koyanagi <yancya@upec.jp>
2023-12-02 02:05:55 +0900

    Add Ep.308

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第307回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第308回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第307回「急ぎ気味近況ラジオ」" \
+       -metadata title="第308回「なんちゃらアイドル七日間戦争」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="七日間戦争の最中からお送りします" \
-        ~/Desktop/307.mp3
+       -metadata TIT3="七日間の戦いをふりかえる" \
+        ~/Desktop/308.mp3
