b53179728f5a6a47942e31c3881f7e6535357b1a
Shinta Koyanagi <yancya@upec.jp>
2023-12-21 21:40:54 +0900

    Add Ep.311

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第310回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第311回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第310回「ブラッシュアップライフ」" \
+       -metadata title="第311回「サブスク無限列車編」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ここ最近もね、たくさん色んな事がありましたね〜" \
-        ~/Desktop/310.mp3
+       -metadata TIT3="ちょっと聞いて下さいよdropboxについて" \
+        ~/Desktop/311.mp3
