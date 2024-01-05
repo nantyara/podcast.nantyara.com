02ae875a097acab8e7da4cd7c8f55a1874a693e0
Shinta Koyanagi <yancya@upec.jp>
2024-01-06 00:40:26 +0900

    Add Ep.313

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第312回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第313回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第312回「年の瀬も年の瀬」" \
+       -metadata title="第313回「あけおめ＆旅行の思い出」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="年末めいてまいりました〜" \
-        ~/Desktop/312.mp3
+       -metadata TIT3="ホーチミンにね" \
+        ~/Desktop/313.mp3
