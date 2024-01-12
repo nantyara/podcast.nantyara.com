cad8534d0495afdc03828248c2bce9ddc036abb9
Shinta Koyanagi <yancya@upec.jp>
2024-01-13 01:07:26 +0900

    Add Ep.314

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第313回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第314回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第313回「あけおめ＆旅行の思い出」" \
+       -metadata title="第314回「あけおめことよろ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ホーチミンにね" \
-        ~/Desktop/313.mp3
+       -metadata TIT3="Tamagotchi Uniで、たまバースに" \
+        ~/Desktop/314.mp3
