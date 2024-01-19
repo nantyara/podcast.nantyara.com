88db5c72d4c2e4f11f270a54a21c641fa1a80b80
Shinta Koyanagi <yancya@upec.jp>
2024-01-20 00:23:42 +0900

    Add Ep.315

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第314回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第315回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第314回「あけおめことよろ」" \
+       -metadata title="第315回「贈り物について考える」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="Tamagotchi Uniで、たまバースに" \
-        ~/Desktop/314.mp3
+       -metadata TIT3="もう10年もやってるみたい" \
+        ~/Desktop/315.mp3
