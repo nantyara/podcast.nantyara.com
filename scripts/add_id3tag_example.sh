a2c4ba0dfb47dd6d9a47b9952b7f8d964c467cc5
Shinta Koyanagi <yancya@upec.jp>
2024-01-26 08:08:59 +0900

    Add Ep.316

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第315回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第316回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第315回「贈り物について考える」" \
+       -metadata title="第316回「傷つかなくていいって話がしたかった」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="もう10年もやってるみたい" \
-        ~/Desktop/315.mp3
+       -metadata TIT3="そろそろ１０周年" \
+        ~/Desktop/316.mp3
