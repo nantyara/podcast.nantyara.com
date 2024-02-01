a15c941404a36f784e3d21343946d03878971bbb
Shinta Koyanagi <yancya@upec.jp>
2024-02-02 00:21:23 +0900

    Add Ep.317

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第316回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第317回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第316回「傷つかなくていいって話がしたかった」" \
+       -metadata title="第317回「10周年ありがとうございました」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="そろそろ１０周年" \
-        ~/Desktop/316.mp3
+       -metadata TIT3="１０周年記念ライブをふりかえります" \
+        ~/Desktop/317.mp3
