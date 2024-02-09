bfc4af725319269cc2910dbbf95da906a0f64d29
Shinta Koyanagi <yancya@upec.jp>
2024-02-09 14:09:22 +0900

    Add Ep.318

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第317回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第318回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第317回「10周年ありがとうございました」" \
+       -metadata title="第318回「お祝いスペシャルウィーク！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="１０周年記念ライブをふりかえります" \
-        ~/Desktop/317.mp3
+       -metadata TIT3="たくさんの人に、お世話になりまして" \
+        ~/Desktop/318.mp3
