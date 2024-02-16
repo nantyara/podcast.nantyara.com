68a66c251394716b7709442119956002273f5a44
Shinta Koyanagi <yancya@upec.jp>
2024-02-17 02:30:50 +0900

    Add Ep.319

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第318回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第319回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第318回「お祝いスペシャルウィーク！」" \
+       -metadata title="第319回「マジで何言ってんのかわからん」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="たくさんの人に、お世話になりまして" \
-        ~/Desktop/318.mp3
+       -metadata TIT3="焼きそばをね？" \
+        ~/Desktop/319.mp3
