5b9612083bd2ca6b21790a65ba7bde5fbb7e21a2
Shinta Koyanagi <yancya@upec.jp>
2024-02-24 16:54:40 +0900

    Add Ep.320

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第319回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第320回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第319回「マジで何言ってんのかわからん」" \
+       -metadata title="第320回「安住紳一郎にくびったけです」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="焼きそばをね？" \
-        ~/Desktop/319.mp3
+       -metadata TIT3="木戸に立ちかけし衣食住" \
+        ~/Desktop/320.mp3
