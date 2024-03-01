eb4223cd2ad8525749372ce630b4b139d8385c76
Shinta Koyanagi <yancya@upec.jp>
2024-03-02 04:10:14 +0900

    Add Ep.321

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第320回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第321回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第320回「安住紳一郎にくびったけです」" \
+       -metadata title="第321回「MBTI診断(って前もやったっけ？)」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="木戸に立ちかけし衣食住" \
-        ~/Desktop/320.mp3
+       -metadata TIT3="やってみましょう" \
+        ~/Desktop/321.mp3
