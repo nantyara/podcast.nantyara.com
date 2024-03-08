4526e727392554f9f6a37fa7a7d6791c474e75e9
Shinta Koyanagi <yancya@upec.jp>
2024-03-08 21:54:51 +0900

    Add Ep.322

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第321回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第322回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第321回「MBTI診断(って前もやったっけ？)」" \
+       -metadata title="第322回「五苓散のおかげで二日酔わなかった気がする」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="やってみましょう" \
-        ~/Desktop/321.mp3
+       -metadata TIT3="目利きの銀次に来ております" \
+        ~/Desktop/322.mp3
