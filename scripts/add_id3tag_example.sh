5838987413f44a39ed4afd1c227bdb95ae18a72c
Shinta Koyanagi <yancya@upec.jp>
2024-03-16 11:40:06 +0900

    Add Ep.323

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第322回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第323回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第322回「五苓散のおかげで二日酔わなかった気がする」" \
+       -metadata title="第323回「羊の話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="目利きの銀次に来ております" \
-        ~/Desktop/322.mp3
+       -metadata TIT3="寒いので、あおはる風呂ラジオ" \
+        ~/Desktop/323.mp3
