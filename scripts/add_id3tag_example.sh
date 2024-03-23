bca536f6c8a3a9f75af720dabdafd426ba5b52e6
Shinta Koyanagi <yancya@upec.jp>
2024-03-23 15:33:48 +0900

    Add Ep.324

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第323回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第324回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第323回「羊の話」" \
+       -metadata title="第324回「裏ピースの意味」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="寒いので、あおはる風呂ラジオ" \
-        ~/Desktop/323.mp3
+       -metadata TIT3="最近セルフネイルにハマってまして" \
+        ~/Desktop/324.mp3
