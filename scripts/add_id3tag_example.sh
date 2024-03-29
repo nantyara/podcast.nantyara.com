65f86fb05ac73fa9362dc068519b53c35b3d0f6a
Shinta Koyanagi <yancya@upec.jp>
2024-03-29 14:15:21 +0900

    Add Ep.325

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第324回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第325回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第324回「裏ピースの意味」" \
+       -metadata title="第325回「牛丼についての考察」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="最近セルフネイルにハマってまして" \
-        ~/Desktop/324.mp3
+       -metadata TIT3="２４時間ウーバーイーツ" \
+        ~/Desktop/325.mp3
