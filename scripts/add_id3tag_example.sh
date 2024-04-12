7247f2ed08cfe68b18847aa8b03a8c773996f360
Shinta Koyanagi <yancya@upec.jp>
2024-04-12 11:40:04 +0900

    Add Ep.327

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第326回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第327回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第326回「食パンをとめるやつはバッグクロージャー」" \
+       -metadata title="第327回「深夜のコソコソ話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="折りたたみ傘を買ったんですが" \
-        ~/Desktop/326.mp3
+       -metadata TIT3="今日はASMR方式で" \
+        ~/Desktop/327.mp3
