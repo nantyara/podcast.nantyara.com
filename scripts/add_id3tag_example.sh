09cd6978b91a43b3eb409ca2fca33ac3ec812182
Shinta Koyanagi <yancya@upec.jp>
2024-04-27 02:39:06 +0900

    Add Ep.329

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第328回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第329回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第328回「お弁当作り回」" \
+       -metadata title="第329回「たこやきうまい」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="キャラ弁全盛期" \
-        ~/Desktop/328.mp3
+       -metadata TIT3="遠征に行ってきました" \
+        ~/Desktop/329.mp3
