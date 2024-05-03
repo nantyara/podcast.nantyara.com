2463c4e897ed6d27b1a99a7eb6770f3717a91327
Shinta Koyanagi <yancya@upec.jp>
2024-05-04 02:25:19 +0900

    Add Ep.330

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第329回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第330回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第329回「たこやきうまい」" \
+       -metadata title="第330回「4月すごかった」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="遠征に行ってきました" \
-        ~/Desktop/329.mp3
+       -metadata TIT3="十日間も休んで、みんな何するの？" \
+        ~/Desktop/330.mp3
