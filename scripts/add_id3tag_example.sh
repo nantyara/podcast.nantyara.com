c17564bee095f180c0861aacbf3c59d31de36be3
Shinta Koyanagi <yancya@upec.jp>
2024-05-04 02:31:41 +0900

    Add Ep.SP014

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第330回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第SP014回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第330回「4月すごかった」" \
+       -metadata title="第SP014回「福岡1日目」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="十日間も休んで、みんな何するの？" \
-        ~/Desktop/330.mp3
+       -metadata TIT3="博多に来てます" \
+        ~/Desktop/sp014.mp3
