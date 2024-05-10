e7050a977f7bf80a9f91bfef40b8372672ac1c42
Shinta Koyanagi <yancya@upec.jp>
2024-05-11 00:54:48 +0900

    Add Ep.331

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第SP014回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第331回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第SP014回「福岡1日目」" \
+       -metadata title="第331回「楽屋の落し物」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="博多に来てます" \
-        ~/Desktop/sp014.mp3
+       -metadata TIT3="不思議だな〜" \
+        ~/Desktop/331.mp3
