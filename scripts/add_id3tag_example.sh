21591461527b191087f0863100bb612b75f440f8
Shinta Koyanagi <yancya@upec.jp>
2024-05-24 10:09:38 +0900

    Add Ep.333

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第332回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第333回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第332回「記憶に結びついてる曲の話」" \
+       -metadata title="第333回「竹ダンス」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="直せない癖みたいなのがあって" \
-        ~/Desktop/332.mp3
+       -metadata TIT3="なくしたわけじゃないんですけど" \
+        ~/Desktop/333.mp3
