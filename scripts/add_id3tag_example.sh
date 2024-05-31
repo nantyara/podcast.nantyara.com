3e6da084beb7a0d517af41c1f8b8c02b104273ef
Shinta Koyanagi <yancya@upec.jp>
2024-06-01 02:53:50 +0900

    Add Ep.334

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第333回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第334回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第333回「竹ダンス」" \
+       -metadata title="第334回「めでたい日」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="なくしたわけじゃないんですけど" \
-        ~/Desktop/333.mp3
+       -metadata TIT3="ブリーチなぁ" \
+        ~/Desktop/334.mp3
