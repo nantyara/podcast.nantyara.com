f0758fa6a8af799c6f88b12dc74c986e5223f827
Shinta Koyanagi <yancya@upec.jp>
2024-06-21 23:59:13 +0900

    Add Ep.337

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第336回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第337回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第336回「なにもかもわからなくなっちゃった」" \
+       -metadata title="第337回「夏、汗。」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="自分の名前とあおはるの誕生日くらいしか覚えてないですね" \
-        ~/Desktop/336.mp3
+       -metadata TIT3="♪" \
+        ~/Desktop/337.mp3
