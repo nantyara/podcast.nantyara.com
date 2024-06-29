074460a54169b709e14d705842818af88b9379e9
Shinta Koyanagi <yancya@upec.jp>
2024-06-29 12:53:24 +0900

    Add Ep.338

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第337回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第338回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第337回「夏、汗。」" \
+       -metadata title="第338回「真・夏、汗。」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="♪" \
-        ~/Desktop/337.mp3
+       -metadata TIT3="こちらが本物の夏、汗です" \
+        ~/Desktop/338.mp3
