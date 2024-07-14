ef8adb4129c3fd755551ee75ef2b22d6a02999ed
Shinta Koyanagi <yancya@upec.jp>
2024-07-14 13:29:04 +0900

    Add Ep.340

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第339回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第340回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第339回「ブバブボ」" \
+       -metadata title="第340回「ハバナイスライフ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="AKIRAの会、優勝しました" \
-        ~/Desktop/339.mp3
+       -metadata TIT3="間違えました" \
+        ~/Desktop/340.mp3
