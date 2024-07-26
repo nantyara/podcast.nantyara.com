7902bc70f419de3a61f207be9b1902f48bba6285
yancya <yancya@pepabo.com>
2024-07-26 15:40:22 +0900

    Add Ep.342

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第341回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第342回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第341回「はにゃほれひれはら」" \
+       -metadata title="第342回「今回のラジオには台本があります。」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="夏の暑さってこんなだったかぁ" \
-        ~/Desktop/341.mp3
+       -metadata TIT3="今回は構成作家がいる『なんちゃラジオ』です" \
+        ~/Desktop/342.mp3
