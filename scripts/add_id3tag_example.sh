7097f1b633d72bdd46a79fae7335f3ac63377421
Shinta Koyanagi <yancya@upec.jp>
2024-08-03 01:48:11 +0900

    Add Ep.343

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第342回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第343回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第342回「今回のラジオには台本があります。」" \
+       -metadata title="第343回「ＱＯＬ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="今回は構成作家がいる『なんちゃラジオ』です" \
-        ~/Desktop/342.mp3
+       -metadata TIT3="２４時間の中で、どう過ごすか" \
+        ~/Desktop/343.mp3
