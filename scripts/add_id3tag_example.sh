1d22b1e4d37617f554ba02a631f90efadcedd8eb
yancya <yancya@pepabo.com>
2024-08-18 04:54:11 +0900

    Add Ep.345

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第344回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第345回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第344回「探し物は何ですか」" \
+       -metadata title="第345回「タイトルは感じ取るタイプの回」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="財布をね" \
-        ~/Desktop/344.mp3
+       -metadata TIT3="大人の文化祭、楽しかった" \
+        ~/Desktop/345.mp3
