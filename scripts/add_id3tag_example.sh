790d7ff4f58f932445391a92a25ad30a2b1880a5
yancya <yancya@pepabo.com>
2024-08-26 11:48:02 +0900

    Add Ep.346

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第345回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第346回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第345回「タイトルは感じ取るタイプの回」" \
+       -metadata title="第346回「なか卯行ったことない」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="大人の文化祭、楽しかった" \
-        ~/Desktop/345.mp3
+       -metadata TIT3="これは世の真面目なアイドルさんからしたら" \
+        ~/Desktop/346.mp3
