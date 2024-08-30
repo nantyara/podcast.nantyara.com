5d7b7c1d07f2bd9674ad39639a8579a3a39d3c2b
Shinta Koyanagi <yancya@upec.jp>
2024-08-31 02:54:44 +0900

    Add Ep.347

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第346回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第347回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第346回「なか卯行ったことない」" \
+       -metadata title="第347回「話すこといっぱいあるよ！！！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="これは世の真面目なアイドルさんからしたら" \
-        ~/Desktop/346.mp3
+       -metadata TIT3="台風、ご安全に" \
+        ~/Desktop/347.mp3
