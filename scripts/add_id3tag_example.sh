31748cf1d54210e45152226e74c90b43ad7c7cd2
Shinta Koyanagi <yancya@upec.jp>
2024-09-07 04:29:07 +0900

    Add Ep.348

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第347回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第348回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第347回「話すこといっぱいあるよ！！！」" \
+       -metadata title="第348回「いいことあっるっぞー」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="台風、ご安全に" \
-        ~/Desktop/347.mp3
+       -metadata TIT3="酷暑っての？しんどみ" \
+        ~/Desktop/348.mp3
