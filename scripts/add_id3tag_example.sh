7b9db6fc1fe382b84f6def9ef9d5d0e359491691
Shinta Koyanagi <yancya@upec.jp>
2024-09-14 15:16:59 +0900

    Add Ep.349

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第348回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第349回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第348回「いいことあっるっぞー」" \
+       -metadata title="第349回「シューティングスター」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="酷暑っての？しんどみ" \
-        ~/Desktop/348.mp3
+       -metadata TIT3="話すこといっぱいあるぞ" \
+        ~/Desktop/349.mp3
