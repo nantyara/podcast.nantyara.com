5b6790f51b5f74450431213b955f101014c9a794
Shinta Koyanagi <yancya@upec.jp>
2024-09-21 06:41:06 +0900

    Add Ep.350

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第349回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第350回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第349回「シューティングスター」" \
+       -metadata title="第350回「信用出来ないもの」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="話すこといっぱいあるぞ" \
-        ~/Desktop/349.mp3
+       -metadata TIT3="肉寿司とかね" \
+        ~/Desktop/350.mp3
