9ca29f969ad0688ce10c5841f461c219c23870fe
Shinta Koyanagi <yancya@upec.jp>
2024-09-28 05:29:58 +0900

    Add Ep.351

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第350回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第351回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第350回「信用出来ないもの」" \
+       -metadata title="第351回「人間性とベーコンは分厚ければ分厚いほどいいですからね」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="肉寿司とかね" \
-        ~/Desktop/350.mp3
+       -metadata TIT3="人間って、いっぱいいるじゃないですか" \
+        ~/Desktop/351.mp3
