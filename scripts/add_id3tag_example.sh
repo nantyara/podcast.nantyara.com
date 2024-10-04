cf52a9fe5f2d4288716d5f3ccdf0eb40f8981828
Shinta Koyanagi <yancya@upec.jp>
2024-10-05 04:42:31 +0900

    Add Ep.352

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第351回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第352回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第351回「人間性とベーコンは分厚ければ分厚いほどいいですからね」" \
+       -metadata title="第352回「雑談回」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="人間って、いっぱいいるじゃないですか" \
-        ~/Desktop/351.mp3
+       -metadata TIT3="喉が疲れた" \
+        ~/Desktop/352.mp3
