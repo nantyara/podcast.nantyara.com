5e9a47b8753efd2b8bca1ee7240e80c8e80aa0d8
Shinta Koyanagi <yancya@upec.jp>
2024-10-12 04:47:00 +0900

    Add Ep.353

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第352回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第353回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第352回「雑談回」" \
+       -metadata title="第353回「パッキングってなんでこんなにも……」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="喉が疲れた" \
-        ~/Desktop/352.mp3
+       -metadata TIT3="まだできてない" \
+        ~/Desktop/353.mp3
