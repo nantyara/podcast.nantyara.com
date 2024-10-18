8d38a7d7586820404f56311b45025d8eafc82a4b
Shinta Koyanagi <yancya@upec.jp>
2024-10-19 02:49:32 +0900

    Add Ep.355

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第354回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第355回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第354回「沖縄1日目」" \
+       -metadata title="第355回「沖縄遠征楽しかったよ回」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="前乗り前乗り〜" \
-        ~/Desktop/354.mp3
+       -metadata TIT3="山羊おいしかったな" \
+        ~/Desktop/355.mp3
