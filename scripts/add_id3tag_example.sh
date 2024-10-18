0067ae8ab3a374b5c3990172ccb72e77b3b796a7
Shinta Koyanagi <yancya@upec.jp>
2024-10-19 02:42:04 +0900

    Add Ep.354

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第353回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第354回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第353回「パッキングってなんでこんなにも……」" \
+       -metadata title="第354回「沖縄1日目」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="まだできてない" \
-        ~/Desktop/353.mp3
+       -metadata TIT3="前乗り前乗り〜" \
+        ~/Desktop/354.mp3
