60aa51f643912faa5d4642a0e083e962c486c93e
Shinta Koyanagi <yancya@upec.jp>
2024-11-03 00:13:09 +0900

    Add Ep.356

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第355回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第356回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第355回「沖縄遠征楽しかったよ回」" \
+       -metadata title="第356回「名古屋だがね」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="山羊おいしかったな" \
-        ~/Desktop/355.mp3
+       -metadata TIT3="名古屋なんですよ" \
+        ~/Desktop/356.mp3
