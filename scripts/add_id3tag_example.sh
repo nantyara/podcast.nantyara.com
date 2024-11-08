05dfe93d062387e9cee75f41f288947dd28ef823
Shinta Koyanagi <yancya@upec.jp>
2024-11-08 13:45:25 +0900

    Add Ep.357

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第356回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第357回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第356回「名古屋だがね」" \
+       -metadata title="第357回「好きな人の話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="名古屋なんですよ" \
-        ~/Desktop/356.mp3
+       -metadata TIT3="久々だなぁ、なんか、ねぇ" \
+        ~/Desktop/357.mp3
