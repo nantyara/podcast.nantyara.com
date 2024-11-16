fefc509e3b2d0f8f89e3cf6c29e3558dd4e590fd
Shinta Koyanagi <yancya@upec.jp>
2024-11-16 23:42:35 +0900

    Add Ep.358

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第357回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第358回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第357回「好きな人の話」" \
+       -metadata title="第358回「思いださないでいて」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="久々だなぁ、なんか、ねぇ" \
-        ~/Desktop/357.mp3
+       -metadata TIT3="もうすぐワンマンライブですね" \
+        ~/Desktop/358.mp3
