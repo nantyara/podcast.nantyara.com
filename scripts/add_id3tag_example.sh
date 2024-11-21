0b5014cd9f143230f3eb48686d0bcb48d763d2a3
Shinta Koyanagi <yancya@upec.jp>
2024-11-21 13:53:48 +0900

    Add Ep.359

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第358回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第359回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第358回「思いださないでいて」" \
+       -metadata title="第359回「どら焼き屋忙しすぎる」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="もうすぐワンマンライブですね" \
-        ~/Desktop/358.mp3
+       -metadata TIT3="ワンマンよかった" \
+        ~/Desktop/359.mp3
