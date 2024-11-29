d4f67b6a3f787597ac83f84fa3c028402eca4b1e
Shinta Koyanagi <yancya@upec.jp>
2024-11-30 03:10:39 +0900

    Add Ep.360

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第359回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第360回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第359回「どら焼き屋忙しすぎる」" \
+       -metadata title="第360回「酔っちゃった」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ワンマンよかった" \
-        ~/Desktop/359.mp3
+       -metadata TIT3="冷蔵庫みたいな部屋より" \
+        ~/Desktop/360.mp3
