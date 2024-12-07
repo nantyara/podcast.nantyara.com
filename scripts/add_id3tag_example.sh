6604b7e9837d150f7910df4abe5db6415d62da5c
Shinta Koyanagi <yancya@upec.jp>
2024-12-07 16:55:52 +0900

    Add Ep.361

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第360回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第361回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第360回「酔っちゃった」" \
+       -metadata title="第361回「眠れぬ夜の友」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="冷蔵庫みたいな部屋より" \
-        ~/Desktop/360.mp3
+       -metadata TIT3="もう年末です" \
+        ~/Desktop/361.mp3
