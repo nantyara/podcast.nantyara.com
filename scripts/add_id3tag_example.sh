55926d719811ec242d71cc4a41e6cbbf4ef52d60
Shinta Koyanagi <yancya@upec.jp>
2024-12-14 01:15:18 +0900

    Add Ep.362

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第361回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第362回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第361回「眠れぬ夜の友」" \
+       -metadata title="第362回「味園ユニバースに立ったあとには」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="もう年末です" \
-        ~/Desktop/361.mp3
+       -metadata TIT3="おめでとう、ありがとう、感無量" \
+        ~/Desktop/362.mp3
