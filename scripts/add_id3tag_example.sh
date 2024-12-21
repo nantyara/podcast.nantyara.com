88ad9a878245e1eeeb9a7bdd60b5adf374070cbe
Shinta Koyanagi <yancya@upec.jp>
2024-12-22 01:31:53 +0900

    Ep.363

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第362回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第363回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第362回「味園ユニバースに立ったあとには」" \
+       -metadata title="第363回「AIと話してみた」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="おめでとう、ありがとう、感無量" \
-        ~/Desktop/362.mp3
+       -metadata TIT3="目指せ渋谷クアトロ" \
+        ~/Desktop/363.mp3
