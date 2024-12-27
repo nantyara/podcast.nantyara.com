5764ea869dfad4519fb12e824d4ecd0be534e1ba
Shinta Koyanagi <yancya@upec.jp>
2024-12-28 00:12:45 +0900

    Ep.364

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第363回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第364回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第363回「AIと話してみた」" \
+       -metadata title="第364回「一年を振り返る」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="目指せ渋谷クアトロ" \
-        ~/Desktop/363.mp3
+       -metadata TIT3="もう年末" \
+        ~/Desktop/364.mp3
