7fff20caf67f208009e91722f62f10739b51ba6e
Shinta Koyanagi <yancya@upec.jp>
2025-01-04 04:24:46 +0900

    Ep.365

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第364回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第365回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第364回「一年を振り返る」" \
+       -metadata title="第365回「あけおめことよろ！に変わる言葉を探してみよう！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="もう年末" \
-        ~/Desktop/364.mp3
+       -metadata TIT3="新年あけおめスペシャル" \
+        ~/Desktop/365.mp3
