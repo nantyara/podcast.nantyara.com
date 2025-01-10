baab6fab50fc31c8d79d0a9cb213ddc57b2f4ce3
Shinta Koyanagi <yancya@upec.jp>
2025-01-11 01:51:13 +0900

    Ep.366

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第365回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第366回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第365回「あけおめことよろ！に変わる言葉を探してみよう！」" \
+       -metadata title="第366回「とるにたらないことを共有しよう」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="新年あけおめスペシャル" \
-        ~/Desktop/365.mp3
+       -metadata TIT3="のんびりできましたか" \
+        ~/Desktop/366.mp3
