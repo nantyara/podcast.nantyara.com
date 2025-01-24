7471d3636094b21997ad96906b7ceeb62eacd92f
Shinta Koyanagi <yancya@upec.jp>
2025-01-25 02:41:08 +0900

    Ep.368

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第367回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第368回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第367回「今日は何の日」" \
+       -metadata title="第368回「あのやり手がゲスト！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="何の日だっけ" \
-        ~/Desktop/367.mp3
+       -metadata TIT3="やり手すぎる" \
+        ~/Desktop/368.mp3
