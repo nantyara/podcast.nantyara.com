36db22ce7e37002fa7f927be90de9151aed52faf
Shinta Koyanagi <yancya@upec.jp>
2025-02-08 00:54:07 +0900

    Ep.370

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第369回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第370回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第369回「誕生日だ！得がしたい！！！」" \
+       -metadata title="第370回「わたし誕生日って好きなんだよな〜」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="誕生日、クーポン届きまくるよね" \
-        ~/Desktop/369.mp3
+       -metadata TIT3="流れてんだろうなぁ、きっと（流れてます）" \
+        ~/Desktop/370.mp3
