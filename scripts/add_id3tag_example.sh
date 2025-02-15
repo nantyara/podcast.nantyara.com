ef83d56d76762802859ef4dab5c0fdcd2b99a9bc
Shinta Koyanagi <yancya@upec.jp>
2025-02-15 23:38:18 +0900

    Ep.371

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第370回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第371回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第370回「わたし誕生日って好きなんだよな〜」" \
+       -metadata title="第371回「その肩幅、道路交通法違反です！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="流れてんだろうなぁ、きっと（流れてます）" \
-        ~/Desktop/370.mp3
+       -metadata TIT3="肩幅ヒロシさんゲスト回です！" \
+        ~/Desktop/371.mp3
