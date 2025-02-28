d411db9a6930281d1e714249dbeb8306b8cbd87a
Shinta Koyanagi <yancya@upec.jp>
2025-03-01 03:51:19 +0900

    Ep.373

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第372回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第373回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第372回「忘れ物ってしたくないよね」" \
+       -metadata title="第373回「大阪何食べた？」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="La.mama の外楽屋回で〜す" \
-        ~/Desktop/372.mp3
+       -metadata TIT3="久しぶりのソロラジオです" \
+        ~/Desktop/373.mp3
