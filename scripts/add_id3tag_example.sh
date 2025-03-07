f104630e44a5ce7cc6d0e8c1bc141dd931629eb8
Shinta Koyanagi <yancya@upec.jp>
2025-03-08 01:34:53 +0900

    Ep.374

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第373回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第374回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第373回「大阪何食べた？」" \
+       -metadata title="第374回「記憶の定着について」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="久しぶりのソロラジオです" \
-        ~/Desktop/373.mp3
+       -metadata TIT3="どこにメモったのかな……" \
+        ~/Desktop/374.mp3
