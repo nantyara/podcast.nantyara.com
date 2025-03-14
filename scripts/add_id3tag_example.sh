05151b0ccecf32fb4094ecc3435877335327a90f
Shinta Koyanagi <yancya@upec.jp>
2025-03-15 01:52:24 +0900

    Ep.375

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第374回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第375回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第374回「記憶の定着について」" \
+       -metadata title="第375回「ランダムなテーマでトークしてみよう！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="どこにメモったのかな……" \
-        ~/Desktop/374.mp3
+       -metadata TIT3="さて、始まってはみたものの" \
+        ~/Desktop/375.mp3
