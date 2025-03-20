cf52b0c7394f471dbd77174496f19394a0a300ce
Shinta Koyanagi <yancya@upec.jp>
2025-03-21 02:01:50 +0900

    Ep.376

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第375回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第376回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第375回「ランダムなテーマでトークしてみよう！」" \
+       -metadata title="第376回「ラジオメモとっててよかった〜」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="さて、始まってはみたものの" \
-        ~/Desktop/375.mp3
+       -metadata TIT3="記憶にないメモがあったときの顔してる" \
+        ~/Desktop/376.mp3
