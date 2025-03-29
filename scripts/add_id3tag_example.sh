f29338025e02cb31054ac72cea9b8dd633d6015c
Shinta Koyanagi <yancya@upec.jp>
2025-03-29 17:43:06 +0900

    Ep.377

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第376回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第377回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第376回「ラジオメモとっててよかった〜」" \
+       -metadata title="第377回「最近のこと」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="記憶にないメモがあったときの顔してる" \
-        ~/Desktop/376.mp3
+       -metadata TIT3="花粉がねぇ" \
+        ~/Desktop/377.mp3
