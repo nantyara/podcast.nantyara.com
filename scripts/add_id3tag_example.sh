199cfbc4cbcd80ca150bc38db78474397cb499f2
Shinta Koyanagi <yancya@upec.jp>
2025-04-05 02:34:57 +0900

    Ep.378

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第377回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第378回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第377回「最近のこと」" \
+       -metadata title="第378回「エイプリルフールとの距離感」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="花粉がねぇ" \
-        ~/Desktop/377.mp3
+       -metadata TIT3="なんか嘘つきましたか？" \
+        ~/Desktop/378.mp3
