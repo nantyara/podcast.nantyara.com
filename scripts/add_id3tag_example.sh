941c4503038a745291345b1abb7db55bd33f3a8e
Shinta Koyanagi <yancya@upec.jp>
2025-04-12 13:35:14 +0900

    Ep.379

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第378回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第379回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第378回「エイプリルフールとの距離感」" \
+       -metadata title="第379回「今年度やりたいこと」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="なんか嘘つきましたか？" \
-        ~/Desktop/378.mp3
+       -metadata TIT3="４月、ある意味で区切りの季節だと思うんです" \
+        ~/Desktop/379.mp3
