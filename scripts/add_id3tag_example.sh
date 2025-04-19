e9f55ed5de5cc51a3ba81828613b618d771e0f81
Shinta Koyanagi <yancya@upec.jp>
2025-04-19 10:31:57 +0900

    Ep.380

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第379回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第380回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第379回「今年度やりたいこと」" \
+       -metadata title="第380回「関白宣言感想戦」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="４月、ある意味で区切りの季節だと思うんです" \
-        ~/Desktop/379.mp3
+       -metadata TIT3="春めいてきた気がしますね。世界が" \
+        ~/Desktop/380.mp3
