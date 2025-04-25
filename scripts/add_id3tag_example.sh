c02ed32eb24d42687c0465ad8e3b1ab343c61f51
Shinta Koyanagi <yancya@upec.jp>
2025-04-26 02:04:52 +0900

    Ep.381

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第380回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第381回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第380回「関白宣言感想戦」" \
+       -metadata title="第381回「自意識　〜電車にて〜」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="春めいてきた気がしますね。世界が" \
-        ~/Desktop/380.mp3
+       -metadata TIT3="今月は忙しかった〜" \
+        ~/Desktop/381.mp3
