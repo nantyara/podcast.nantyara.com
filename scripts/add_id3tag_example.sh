b32790d294d201d48cd40f49d11d43540bd58a89
Shinta Koyanagi <yancya@upec.jp>
2025-05-02 23:45:07 +0900

    Ep.382

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第381回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第382回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第381回「自意識　〜電車にて〜」" \
+       -metadata title="第382回「寿限無」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="今月は忙しかった〜" \
-        ~/Desktop/381.mp3
+       -metadata TIT3="父親が誕生日を迎えました" \
+        ~/Desktop/382.mp3
