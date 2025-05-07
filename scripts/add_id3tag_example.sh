f13b7797e5aaffc489d0870bee11b841f85f5bb6
Shinta Koyanagi <yancya@upec.jp>
2025-05-08 01:52:46 +0900

    Ep.383

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第382回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第383回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第382回「寿限無」" \
+       -metadata title="第383回「オタマトーンやってみた」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="父親が誕生日を迎えました" \
-        ~/Desktop/382.mp3
+       -metadata TIT3="しゃべってるみたいでかわいいね" \
+        ~/Desktop/383.mp3
