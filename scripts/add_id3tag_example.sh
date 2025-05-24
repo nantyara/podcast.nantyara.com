46f017b993f5f4a327aaed5aa093981d0a0ad580
Shinta Koyanagi <yancya@upec.jp>
2025-05-24 18:54:52 +0900

    Ep.385

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第384回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第385回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第384回「人生をよりよくする方法」" \
+       -metadata title="第385回「洗濯物を、畳むよ！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="今起きました〜" \
-        ~/Desktop/384.mp3
+       -metadata TIT3="夏が始まりまして" \
+        ~/Desktop/385.mp3
