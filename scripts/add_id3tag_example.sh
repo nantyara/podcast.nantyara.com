3b01831768fe01a55b31f2a9cdfa458c9b895010
Shinta Koyanagi <yancya@upec.jp>
2025-06-01 06:25:26 +0900

    Ep.386

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第385回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第386回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第385回「洗濯物を、畳むよ！」" \
+       -metadata title="第386回「微効率厨」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="夏が始まりまして" \
-        ~/Desktop/385.mp3
+       -metadata TIT3="顔が近いな" \
+        ~/Desktop/386.mp3
