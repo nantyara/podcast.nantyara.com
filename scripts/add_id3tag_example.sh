6502f8f8268aca091765d722e814e3254181755b
Shinta Koyanagi <yancya@upec.jp>
2025-06-06 18:10:15 +0900

    Ep.387

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第386回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第387回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第386回「微効率厨」" \
+       -metadata title="第387回「グッド・バイ(Good buy)」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="顔が近いな" \
-        ~/Desktop/386.mp3
+       -metadata TIT3="鼻がねぇ、詰まってて" \
+        ~/Desktop/387.mp3
