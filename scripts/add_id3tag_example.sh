c942c90523f8e025fda0f3d4847d8927ee42cd8a
Shinta Koyanagi <yancya@upec.jp>
2025-06-21 23:49:40 +0900

    Ep.389

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第388回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第389回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第388回「人生の常識とビジネス横文字知らないマン」" \
+       -metadata title="第389回「歩く」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="そろそろ夏至です" \
-        ~/Desktop/388.mp3
+       -metadata TIT3="休みです" \
+        ~/Desktop/389.mp3
