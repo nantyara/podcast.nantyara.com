40375cbe7d6c7f4047cede2bf56a1de6c0e05986
Shinta Koyanagi <yancya@upec.jp>
2025-06-28 16:44:22 +0900

    Ep.390

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第389回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第390回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第389回「歩く」" \
+       -metadata title="第390回「For sale baby shoes never worn」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="休みです" \
-        ~/Desktop/389.mp3
+       -metadata TIT3="悩みがあります" \
+        ~/Desktop/390.mp3
