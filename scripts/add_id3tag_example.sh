95cb8a683f698e7f89e41d61ec3a6aa3ffd03a0a
Shinta Koyanagi <yancya@upec.jp>
2025-07-05 09:30:06 +0900

    Ep.391

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第390回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第391回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第390回「For sale baby shoes never worn」" \
+       -metadata title="第391回「ブルベ夏 アンチ冬」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="悩みがあります" \
-        ~/Desktop/390.mp3
+       -metadata TIT3="許して下さい。あおはるです" \
+        ~/Desktop/391.mp3
