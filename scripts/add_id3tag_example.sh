5560bec0a7b922500c21ddff86ea61e1059e59b3
Shinta Koyanagi <yancya@upec.jp>
2025-07-12 22:33:34 +0900

    Ep.392

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第391回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第392回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第391回「ブルベ夏 アンチ冬」" \
+       -metadata title="第392回「夏って感じだ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="許して下さい。あおはるです" \
-        ~/Desktop/391.mp3
+       -metadata TIT3="どごぉん、ざばぁ" \
+        ~/Desktop/392.mp3
