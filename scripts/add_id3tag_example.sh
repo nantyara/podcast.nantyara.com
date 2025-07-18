7321a20672aa209a736edfdca855d6f1543644b4
Shinta Koyanagi <yancya@upec.jp>
2025-07-18 15:03:07 +0900

    Ep.393

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第392回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第393回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第392回「夏って感じだ」" \
+       -metadata title="第393回「整体をやめたい」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="どごぉん、ざばぁ" \
-        ~/Desktop/392.mp3
+       -metadata TIT3="行きたくないんですよぉ" \
+        ~/Desktop/393.mp3
