a300efbe08b0fee8d494f6a4270a05a906670306
Shinta Koyanagi <yancya@upec.jp>
2025-07-26 12:58:12 +0900

    Ep.394

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第393回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第394回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第393回「整体をやめたい」" \
+       -metadata title="第394回「お江戸にトビマストビマス！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="行きたくないんですよぉ" \
-        ~/Desktop/393.mp3
+       -metadata TIT3="なんか、咆哮をあげてしまいましたが" \
+        ~/Desktop/394.mp3
