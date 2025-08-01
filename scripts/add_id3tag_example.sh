72baf45c66b3c5d2ef9a7e5f4df61e5b13434d91
Shinta Koyanagi <yancya@upec.jp>
2025-08-01 14:46:22 +0900

    Ep.395

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第394回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第395回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第394回「お江戸にトビマストビマス！」" \
+       -metadata title="第395回「しりとりやる」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="なんか、咆哮をあげてしまいましたが" \
-        ~/Desktop/394.mp3
+       -metadata TIT3="とりあえず一戦、やらせていただきます" \
+        ~/Desktop/395.mp3
