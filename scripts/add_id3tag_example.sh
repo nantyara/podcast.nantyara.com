396ba33c0bbc1bab60cc20db10f6b51bced8fbcc
Shinta Koyanagi <yancya@upec.jp>
2025-08-09 03:51:09 +0900

    Ep.396

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第395回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第396回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第395回「しりとりやる」" \
+       -metadata title="第396回「狂気の5daysを終えて」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="とりあえず一戦、やらせていただきます" \
-        ~/Desktop/395.mp3
+       -metadata TIT3="帰り道に八百屋があるんですよ" \
+        ~/Desktop/396.mp3
