dee41df327b9e2d0fb199b7015d412568deef6b0
Shinta Koyanagi <yancya@upec.jp>
2025-08-15 03:55:14 +0900

    Ep.397

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第396回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第397回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第396回「狂気の5daysを終えて」" \
+       -metadata title="第397回「帰省したンゴ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="帰り道に八百屋があるんですよ" \
-        ~/Desktop/396.mp3
+       -metadata TIT3="ドモドモドモヤホヤホヤホヤホ" \
+        ~/Desktop/397.mp3
