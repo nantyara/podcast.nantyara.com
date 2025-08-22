e1d013e0b2b2404c9d3c3e6219075a1cb63e3f1c
Shinta Koyanagi <yancya@upec.jp>
2025-08-23 07:59:27 +0900

    Ep.398

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第397回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第398回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第397回「帰省したンゴ」" \
+       -metadata title="第398回「福岡とサプリメント」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ドモドモドモヤホヤホヤホヤホ" \
-        ~/Desktop/397.mp3
+       -metadata TIT3="チチチチチチチチ、ダン！" \
+        ~/Desktop/398.mp3
