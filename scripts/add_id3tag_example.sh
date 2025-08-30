782bef542a7bd0841fe0c9b919a361ec8a8892b8
Shinta Koyanagi <yancya@upec.jp>
2025-08-30 17:49:21 +0900

    Ep.399

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第398回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第399回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第398回「福岡とサプリメント」" \
+       -metadata title="第399回「ベロの位置ってどこが正しいんだ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="チチチチチチチチ、ダン！" \
-        ~/Desktop/398.mp3
+       -metadata TIT3="こんにちは〜！（ウィスパーボイス）（レッドゾーン）" \
+        ~/Desktop/399.mp3
