323c0281885afed124776fab5bd4dc93e3f65b98
Shinta Koyanagi <yancya@upec.jp>
2025-09-06 01:47:47 +0900

    Ep.400

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第399回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第400回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第399回「ベロの位置ってどこが正しいんだ」" \
+       -metadata title="第400回「【神回】400回目はさる小をキーステーションにお送りします【公開録音】」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="こんにちは〜！（ウィスパーボイス）（レッドゾーン）" \
-        ~/Desktop/399.mp3
+       -metadata TIT3="なんちゃラジオって、そうだったの" \
+        ~/Desktop/400.mp3
