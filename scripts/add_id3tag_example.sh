ab336e17b47d2d4e5fbe842a52606891d87e1f33
Shinta Koyanagi <yancya@upec.jp>
2025-09-12 03:06:23 +0900

    Ep.401

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第400回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第401回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第400回「【神回】400回目はさる小をキーステーションにお送りします【公開録音】」" \
+       -metadata title="第401回「おいでよ！さる小の3日間！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="なんちゃラジオって、そうだったの" \
-        ~/Desktop/400.mp3
+       -metadata TIT3="それを言うならプロンプトですね" \
+        ~/Desktop/401.mp3
