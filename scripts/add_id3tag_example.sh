88d4f62eaa452ccbd7bd92c597f16222f8ad29cc
Shinta Koyanagi <yancya@upec.jp>
2025-09-27 02:37:53 +0900

    Ep.403

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第402回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第403回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第402回「健康女子」" \
+       -metadata title="第403回「月を見て君を想うのか、君を想うから月を見るのか。」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
        -metadata TIT3="びっくらポンってね" \
-        ~/Desktop/402.mp3
+        ~/Desktop/403.mp3
