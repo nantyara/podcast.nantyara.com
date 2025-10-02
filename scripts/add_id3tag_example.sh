1b7146ead82f29ae9d3fbaee7d6c88bef9f86511
Shinta Koyanagi <yancya@upec.jp>
2025-10-02 19:00:48 +0900

    Add Ep.404

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第403回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第404回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第403回「月を見て君を想うのか、君を想うから月を見るのか。」" \
+       -metadata title="第404回「ラブタイプ♡」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="びっくらポンってね" \
-        ~/Desktop/403.mp3
+       -metadata TIT3="さてさて、こ、今回も、はじめていきましょう" \
+        ~/Desktop/404.mp3
