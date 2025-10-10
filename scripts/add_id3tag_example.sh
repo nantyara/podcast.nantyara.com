39e318d856756879de95200b39df4d33fd59a066
Shinta Koyanagi <yancya@upec.jp>
2025-10-11 01:15:45 +0900

    Add Ep.405

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第404回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第405回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第404回「ラブタイプ♡」" \
+       -metadata title="第405回「ガリガリくんのあたりをいつも期待している」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="さてさて、こ、今回も、はじめていきましょう" \
-        ~/Desktop/404.mp3
+       -metadata TIT3="今日、すんごい涼しかったわ" \
+        ~/Desktop/405.mp3
