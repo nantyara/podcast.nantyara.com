e64d47bd806ea35526a846acb8857c90386d7179
Shinta Koyanagi <yancya@upec.jp>
2025-10-17 22:37:21 +0900

    Add Ep.406

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第405回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第406回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第405回「ガリガリくんのあたりをいつも期待している」" \
+       -metadata title="第406回「普通に近況」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="今日、すんごい涼しかったわ" \
-        ~/Desktop/405.mp3
+       -metadata TIT3="サブスクのねぇ" \
+        ~/Desktop/406.mp3
