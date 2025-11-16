66238926767b3987f5a531fb9f9409520e1deaec
Shinta Koyanagi <yancya@upec.jp>
2025-11-16 20:45:42 +0900

    Add Ep.410

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第409回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第410回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第409回「干支1周て〜〜〜〜〜」" \
+       -metadata title="第410回「風呂回」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="タンミョン届いてる〜" \
-        ~/Desktop/409.mp3
+       -metadata TIT3="今日はこれで勘弁してください" \
+        ~/Desktop/410.mp3
