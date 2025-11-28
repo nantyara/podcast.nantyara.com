d272c266eff570efe45bebc94f608aacfcc00739
Shinta Koyanagi <yancya@upec.jp>
2025-11-29 04:09:59 +0900

    Add Ep.412

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第411回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第412回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第411回「ピーカンナッツのチョコレート」" \
+       -metadata title="第412回「いろんな街のオススメのお店」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="冬来る" \
-        ~/Desktop/411.mp3
+       -metadata TIT3="東京都北区赤羽です" \
+        ~/Desktop/412.mp3
