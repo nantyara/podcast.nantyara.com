51e2693538c0480e4543d1f2f1de727c55f58e90
Shinta Koyanagi <yancya@upec.jp>
2025-12-07 03:41:49 +0900

    Add Ep.414

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第413回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第414回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第413回「都市伝説と自己開示」" \
+       -metadata title="第414回「オススメのお弁当」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ウォーターフロスはいいぞ" \
-        ~/Desktop/413.mp3
+       -metadata TIT3="オススメの外食スポットです" \
+        ~/Desktop/414.mp3
