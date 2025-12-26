f61e5cd87be8b50c5c0d9fd02a948ab7d6a54cd8
Shinta Koyanagi <yancya@upec.jp>
2025-12-26 23:06:33 +0900

    Add Ep.417

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第416回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第417回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第416回「今年も大変お世話になりました！」" \
+       -metadata title="第417回「東京と漫画の話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="福岡ありがとうございましたっ" \
-        ~/Desktop/416.mp3
+       -metadata TIT3="メリクリ〜" \
+        ~/Desktop/417.mp3
