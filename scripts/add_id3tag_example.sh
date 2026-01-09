69cefce5ec4450b59be63fcb158198c4cf7f6ce4
Shinta Koyanagi <yancya@upec.jp>
2026-01-09 23:57:22 +0900

    Add Ep.418

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第417回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第418回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第417回「東京と漫画の話」" \
+       -metadata title="第418回「新年明けましておめでとう午ざいます」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="メリクリ〜" \
-        ~/Desktop/417.mp3
+       -metadata TIT3="超絶技巧ベース教本をやることです" \
+        ~/Desktop/418.mp3
