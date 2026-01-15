68f82d9e1ba67568a508b95762401eefc93eadb7
Shinta Koyanagi <yancya@upec.jp>
2026-01-16 05:16:38 +0900

    Add Ep.419

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第418回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第419回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第418回「新年明けましておめでとう午ざいます」" \
+       -metadata title="第419回「芸人のラジオ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="超絶技巧ベース教本をやることです" \
-        ~/Desktop/418.mp3
+       -metadata TIT3="やっと新年の話が出来る" \
+        ~/Desktop/419.mp3
