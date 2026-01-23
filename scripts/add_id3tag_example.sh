1412cf07d16672974cf2becae11f4f25b30864ce
Shinta Koyanagi <yancya@upec.jp>
2026-01-23 14:15:53 +0900

    Add Ep.420

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第419回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第420回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第419回「芸人のラジオ」" \
+       -metadata title="第420回「私が恐れているもの回」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="やっと新年の話が出来る" \
-        ~/Desktop/419.mp3
+       -metadata TIT3="ヒートショック好きすぎる" \
+        ~/Desktop/420.mp3
