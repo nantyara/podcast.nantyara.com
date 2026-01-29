e9d00d3ee50f786f51817ce3fe3cb5ebb75bfcdb
Shinta Koyanagi <yancya@upec.jp>
2026-01-30 03:47:45 +0900

    Add Ep.421

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第420回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第421回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第420回「私が恐れているもの回」" \
+       -metadata title="第421回「連続主催ありがとうとか」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ヒートショック好きすぎる" \
-        ~/Desktop/420.mp3
+       -metadata TIT3="2026年1月27,28,29日、ありがとうございました" \
+        ~/Desktop/421.mp3
