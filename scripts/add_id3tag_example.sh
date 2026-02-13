92e3d052a8883efa2db6950a9d6e3efab62cf433
Shinta Koyanagi <yancya@upec.jp>
2026-02-13 23:08:49 +0900

    Add Ep.423

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第422回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第423回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第422回「口腔内でドリルが超高速回転してるって冷静に考えて怖過ぎ」" \
+       -metadata title="第423回「風邪ひいたけど治った」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="気づいちゃいました" \
-        ~/Desktop/422.mp3
+       -metadata TIT3="流行りのＢでした" \
+       ~/Desktop/423.mp3
