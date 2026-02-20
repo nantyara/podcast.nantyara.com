36d804771689f14cee3572c8190a10318168e54e
Shinta Koyanagi <yancya@upec.jp>
2026-02-20 20:38:57 +0900

    Add Ep.424

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第423回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第424回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第423回「風邪ひいたけど治った」" \
+       -metadata title="第424回「お誕生日特別ラジオ！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="流行りのＢでした" \
-       ~/Desktop/423.mp3
+       -metadata TIT3="時空、超えてみました" \
+       ~/Desktop/424.mp3
