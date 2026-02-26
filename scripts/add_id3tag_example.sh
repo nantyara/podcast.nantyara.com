fc946b162588770d6d4a84953078055ac0c8b584
Shinta Koyanagi <yancya@upec.jp>
2026-02-27 05:57:37 +0900

    Add Ep.425

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第424回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第425回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第424回「お誕生日特別ラジオ！」" \
+       -metadata title="第425回「花粉症かつてんてこまい」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="時空、超えてみました" \
-       ~/Desktop/424.mp3
+       -metadata TIT3="道理でって思った" \
+       ~/Desktop/425.mp3
