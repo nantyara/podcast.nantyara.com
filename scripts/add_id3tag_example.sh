08aacbb94c9a24c905f94caed943b6f6acb3865d
Shinta Koyanagi <yancya@upec.jp>
2026-03-07 00:53:37 +0900

    Add Ep.426

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第425回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第426回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第425回「花粉症かつてんてこまい」" \
+       -metadata title="第426回「Tiny teeth take time to treat」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="道理でって思った" \
-       ~/Desktop/425.mp3
+       -metadata TIT3="口内環境がね" \
+       ~/Desktop/426.mp3
