82d398614bd766aa74c8efdebdc97b53754ba031
Shinta Koyanagi <yancya@upec.jp>
2026-03-20 01:38:53 +0900

    Add Ep.428

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第427回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第428回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第427回「行ったことないチェーン店」" \
+       -metadata title="第428回「私だってできると信じていたい」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="逆にね" \
-       ~/Desktop/427.mp3
+       -metadata TIT3="ついに治療が終わりました" \
+       ~/Desktop/428.mp3
