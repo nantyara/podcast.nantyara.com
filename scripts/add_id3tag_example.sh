54de0c594899d9c14c689567e78205a6a1717602
Shinta Koyanagi <yancya@upec.jp>
2026-03-27 02:08:49 +0900

    Update Ep.429

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第428回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第429回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第428回「私だってできると信じていたい」" \
+       -metadata title="第429回「エピソードトークを上手く話したい！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ついに治療が終わりました" \
-       ~/Desktop/428.mp3
+       -metadata TIT3="どうですか？" \
+       ~/Desktop/429.mp3
