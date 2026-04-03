a46f7a0e8b80259ad6c1c31ed7e36c4a8ef0c7db
Shinta Koyanagi <yancya@upec.jp>
2026-04-03 23:01:22 +0900

    Update Ep.430

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第429回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第430回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第429回「エピソードトークを上手く話したい！」" \
+       -metadata title="第430回「やるぞやるぞやるぞ〜！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="どうですか？" \
-       ~/Desktop/429.mp3
+       -metadata TIT3="マチョマチョ、マチョマチョ" \
+       ~/Desktop/430.mp3
