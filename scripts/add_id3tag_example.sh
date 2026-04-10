ba436094857a114378dfcffc6037efed0952987d
Shinta Koyanagi <yancya@upec.jp>
2026-04-11 01:51:06 +0900

    Update Ep.431

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第430回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第431回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第430回「やるぞやるぞやるぞ〜！」" \
+       -metadata title="第431回「2026年 ベストバイ！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="マチョマチョ、マチョマチョ" \
-       ~/Desktop/430.mp3
+       -metadata TIT3="ご紹介していこうと思います" \
+       ~/Desktop/431.mp3
