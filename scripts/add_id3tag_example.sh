217b07e3085c01b0f4dd6946164b85b4a87b99da
Shinta Koyanagi <yancya@upec.jp>
2026-04-24 19:07:46 +0900

    Update Ep.433

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第432回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第433回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第432回「質感ラジオ」" \
+       -metadata title="第433回「大阪旅行談」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="1st ソロワンマンへご来場ありがとうございました" \
-       ~/Desktop/432.mp3
+       -metadata TIT3="ゴロゴロ、ゴロゴロと転がり" \
+       ~/Desktop/433.mp3
