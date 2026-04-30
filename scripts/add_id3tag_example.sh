2caeff10815122a0d6f0878981b133b5056154e7
Shinta Koyanagi <yancya@upec.jp>
2026-05-01 07:45:14 +0900

    Update Ep.434

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第433回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第434回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第433回「大阪旅行談」" \
+       -metadata title="第434回「また歯の話で恐縮です」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ゴロゴロ、ゴロゴロと転がり" \
-       ~/Desktop/433.mp3
+       -metadata TIT3="大阪遠征ありがとうございました" \
+       ~/Desktop/434.mp3
