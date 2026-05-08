dd18b23123281ed5d965bbd45678654cc45fce12
Shinta Koyanagi <yancya@upec.jp>
2026-05-09 06:24:53 +0900

    Update Ep.435

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第434回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第435回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第434回「また歯の話で恐縮です」" \
+       -metadata title="第434回「オチはないけどなんかいい話」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="大阪遠征ありがとうございました" \
-       ~/Desktop/434.mp3
+       -metadata TIT3="死期を覚ったらね" \
+       ~/Desktop/435.mp3
