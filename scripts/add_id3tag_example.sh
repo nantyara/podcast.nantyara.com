7ca0fc79fe4b13daa5a443a8d5039ca833e9f0c1
Shinta Koyanagi <yancya@upec.jp>
2026-05-15 21:12:25 +0900

    Update Ep.436

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第435回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第436回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第434回「オチはないけどなんかいい話」" \
+       -metadata title="第436回「乱数調整的自炊」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="死期を覚ったらね" \
-       ~/Desktop/435.mp3
+       -metadata TIT3="眠すぎる" \
+       ~/Desktop/436.mp3
