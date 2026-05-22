c1b0ff8825c6e1b95b61b1578a0fab03ef94ee56
Shinta Koyanagi <yancya@upec.jp>
2026-05-23 00:36:13 +0900

    Update Ep.437

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第436回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第437回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第436回「乱数調整的自炊」" \
+       -metadata title="第437回「してしまったミスとその反省」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="眠すぎる" \
-       ~/Desktop/436.mp3
+       -metadata TIT3="厚木じゃないよ厚着だよ" \
+       ~/Desktop/437.mp3
