b0357dc9d0a50b12b3bee2eafb617d414a1317ae
Shinta Koyanagi <yancya@upec.jp>
2026-05-29 03:02:00 +0900

    Update Ep.438

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第437回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第438回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第437回「してしまったミスとその反省」" \
+       -metadata title="第438回「取り留めもなく話すときのオノマトペとは」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="厚木じゃないよ厚着だよ" \
-       ~/Desktop/437.mp3
+       -metadata TIT3="色んな事があったんじゃあないですか？" \
+       ~/Desktop/438.mp3
