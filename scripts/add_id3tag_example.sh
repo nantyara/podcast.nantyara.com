a3efcc8bb664ef3a39b184efc3a16e2b3c4f85c0
Shinta Koyanagi <yancya@upec.jp>
2026-06-13 01:11:53 +0900

    Update Ep.440

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第439回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第440回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第439回「中学生の時に読んでた本」" \
+       -metadata title="第440回「7.8.9.1.2.3」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ミライザカええな" \
-       ~/Desktop/439.mp3
+       -metadata TIT3="ナイトガード忘れるとそうなる" \
+       ~/Desktop/440.mp3
