ddfa2a695b97e245ee58c69fdedaac4290204cf4
Shinta Koyanagi <yancya@upec.jp>
2026-06-20 03:34:36 +0900

    Update Ep.441

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第440回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第441回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第440回「7.8.9.1.2.3」" \
+       -metadata title="第441回「塊魂は顔が浮腫む」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ナイトガード忘れるとそうなる" \
-       ~/Desktop/440.mp3
+       -metadata TIT3="塊魂が好きすぎる" \
+       ~/Desktop/441.mp3
