6db661c6f05e475589f8059ac0e04583d3f8b33a
Shinta Koyanagi <yancya@upec.jp>
2026-06-26 18:51:00 +0900

    Update Ep.442

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第441回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第442回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第441回「塊魂は顔が浮腫む」" \
+       -metadata title="第442回「夢で食べたあのパンが食べたい」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="塊魂が好きすぎる" \
-       ~/Desktop/441.mp3
+       -metadata TIT3="各々うまいことやっといてください" \
+       ~/Desktop/442.mp3
