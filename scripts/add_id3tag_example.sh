685170e2a57f5794bc2710a954c10e20e11872a7
Shinta Koyanagi <yancya@upec.jp>
2026-07-03 10:47:02 +0900

    Update Ep.443

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第442回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第443回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第442回「夢で食べたあのパンが食べたい」" \
+       -metadata title="第443回「髪切ったよん」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="各々うまいことやっといてください" \
-       ~/Desktop/442.mp3
+       -metadata TIT3="重力に逆らってます" \
+       ~/Desktop/443.mp3
