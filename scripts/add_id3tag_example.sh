9996d903eb7ebb729ad8539bdced178e72273d5e
Shinta Koyanagi <yancya@upec.jp>
2026-07-10 18:41:36 +0900

    Update Ep.444

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第443回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第444回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第443回「髪切ったよん」" \
+       -metadata title="第444回「なんだかんだツイている女」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="重力に逆らってます" \
-       ~/Desktop/443.mp3
+       -metadata TIT3="チュパチュパしてまう" \
+       ~/Desktop/444.mp3
