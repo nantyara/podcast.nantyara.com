396ffd3153544b0f402daa092832782632f58dec
Shinta Koyanagi <yancya@upec.jp>
2026-07-16 23:12:23 +0900

    Update Ep.445

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第444回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第445回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第444回「なんだかんだツイている女」" \
+       -metadata title="第445回「新潟旅行のはなし」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="チュパチュパしてまう" \
-       ~/Desktop/444.mp3
+       -metadata TIT3="メンタルヘルスマネジメントのスキルを使われたかも" \
+       ~/Desktop/445.mp3
