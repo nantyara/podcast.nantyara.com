2256cf5cdeca583c8ad636a3667d14c59b631145
Shinta Koyanagi <yancya@upec.jp>
2026-07-25 13:29:52 +0900

    Update Ep.446

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第445回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第446回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第445回「新潟旅行のはなし」" \
+       -metadata title="第446回「キュートな球体で」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="メンタルヘルスマネジメントのスキルを使われたかも" \
-       ~/Desktop/445.mp3
+       -metadata TIT3="梅雨明けましたね" \
+       ~/Desktop/446.mp3
