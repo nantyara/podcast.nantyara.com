fbd8e12c188c33b3650f56be19af0fdf18324e9a
Shinta Koyanagi <yancya@upec.jp>
2026-08-07 10:03:03 +0900

    Update Ep.448

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第447回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第448回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第447回「夏をのりきろー」" \
+       -metadata title="第448回「上半期ベストバイ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="海行ってますか？プール行ってますか？" \
-       ~/Desktop/447.mp3
+       -metadata TIT3="ZERUのワンデーカラコン、色はウォーターシルバーもかわいくて買ってよかったけど、開けづらい" \
+       ~/Desktop/448.mp3
