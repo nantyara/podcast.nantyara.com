a2481f352d08c19d810c748bc8e10ece9b84b010
Shinta Koyanagi <yancya@upec.jp>
2026-08-14 02:20:54 +0900

    Update Ep.449

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第448回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第449回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第448回「上半期ベストバイ」" \
+       -metadata title="第449回「ごめんスマホ風呂に落とした」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ZERUのワンデーカラコン、色はウォーターシルバーもかわいくて買ってよかったけど、開けづらい" \
-       ~/Desktop/448.mp3
+       -metadata TIT3="ASMRっ" \
+       ~/Desktop/449.mp3
