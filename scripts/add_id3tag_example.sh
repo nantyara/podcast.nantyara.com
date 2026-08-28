390ef9a5d034a963452669cf16285af54f78775a
Shinta Koyanagi <yancya@upec.jp>
2026-08-29 02:15:35 +0900

    Update Ep.451

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第450回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第451回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第450回「すみません、かみまみた」" \
+       -metadata title="第451回「早起ききらい」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="ラジオ途中にマウスピースをつけて、滑舌が悪くなったと笑っていますが、冒頭から滑舌悪いです" \
-       ~/Desktop/450.mp3
+       -metadata TIT3="なんちゃらアイドルのラジオ、それが、なんちゃラジオ" \
+       ~/Desktop/451.mp3
