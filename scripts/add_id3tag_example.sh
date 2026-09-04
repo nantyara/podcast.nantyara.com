433979f2601b8dc7952cef180200c8bfbe6c9d7b
Shinta Koyanagi <yancya@upec.jp>
2026-09-05 00:17:34 +0900

    Update Ep.452

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第451回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第452回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第451回「早起ききらい」" \
+       -metadata title="第452回「人生は経験や！」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="なんちゃらアイドルのラジオ、それが、なんちゃラジオ" \
-       ~/Desktop/451.mp3
+       -metadata TIT3="「御茶海マミ」って言いづらいな？" \
+       ~/Desktop/452.mp3
