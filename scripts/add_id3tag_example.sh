adc323d9730f253744c6794667697672a0666fba
Shinta Koyanagi <yancya@upec.jp>
2026-09-11 01:40:33 +0900

    Update Ep.453

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第452回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第453回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第452回「人生は経験や！」" \
+       -metadata title="第453回「買ってよかったもの。美味しくなかったジュース。」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="「御茶海マミ」って言いづらいな？" \
-       ~/Desktop/452.mp3
+       -metadata TIT3="あるよね人生って、いろいろ" \
+       ~/Desktop/453.mp3
