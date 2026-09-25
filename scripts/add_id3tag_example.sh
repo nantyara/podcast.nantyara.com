278c55f63f9a881aa797f882c4509da71d93682d
Shinta Koyanagi <yancya@upec.jp>
2026-09-25 16:52:34 +0900

    Update Ep.455

--- scripts/add_id3tag_example.sh
+++ scripts/add_id3tag_example.sh
@@ -1,13 +1,13 @@
-ffmpeg -i ~/Desktop/なんちゃラジオ第454回.mp3 \
+ffmpeg -i ~/Desktop/なんちゃラジオ第455回.mp3 \
        -i $(pwd)/images/artwork.jpg \
        -map 0 \
        -map 1 \
        -c copy \
        -c:v:1 mjpeg \
        -id3v2_version 3 \
-       -metadata title="第454回「刑法223条、204条・205条」" \
+       -metadata title="第455回「マイクロブタ触り逃げ」" \
        -metadata genre="Podcast" \
        -metadata artist="なんちゃらアイドル" \
        -metadata album="なんちゃラジオ" \
-       -metadata TIT3="なんて愚かなんだろうって" \
-       ~/Desktop/454.mp3
+       -metadata TIT3="五連休でしたね五連休" \
+       ~/Desktop/455.mp3
